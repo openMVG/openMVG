c36c154db5c2effd5e2a114a0833a1efc210fa9b
pmoulon <pmoulon@gmail.com>
2014-01-19 10:54:09 +0100

    OpenSource implementation of the ICCV13 paper titled 'Global Fusion of Relative Motions for Robust, Accurate and Scalable Structure from Motion. Authors: Pierre MOULON, Pascal MONASSE and Renaud MARLET.'. #60

---
+++ src/software/globalSfM/ImageCollection_E_ACRobust.hpp
@@ -0,0 +1,68 @@
+
+// Copyright (c) 2012, 2013, 2014 Pierre MOULON.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "openMVG/multiview/solver_essential_kernel.hpp"
+#include "openMVG/robust_estimation/robust_estimator_ACRansac.hpp"
+#include "openMVG/robust_estimation/robust_estimator_ACRansacKernelAdaptator.hpp"
+
+using namespace openMVG;
+using namespace openMVG::robust;
+
+#include <limits>
+
+//-- A contrario Functor to filter putative corresponding points
+//--  thanks estimation of the essential matrix.
+//- Suppose that all image have the same K matrix
+struct GeometricFilter_EMatrix_AC
+{
+  GeometricFilter_EMatrix_AC(
+    const Mat3 & K,
+    double dPrecision = std::numeric_limits<double>::infinity(),
+    size_t iteration = 4096)
+    : m_dPrecision(dPrecision), m_stIteration(iteration), m_K(K) {};
+
+  /// Robust fitting of the ESSENTIAL matrix
+  void Fit(
+    const Mat & xA,
+    const std::pair<size_t, size_t> & imgSizeA,
+    const Mat & xB,
+    const std::pair<size_t, size_t> & imgSizeB,
+    std::vector<size_t> & vec_inliers) const
+  {
+    vec_inliers.resize(0);
+
+    // Define the AContrario adapted Essential matrix solver
+    typedef ACKernelAdaptorEssential<
+        openMVG::essential::kernel::FivePointKernel,
+        openMVG::fundamental::kernel::EpipolarDistanceError,
+        UnnormalizerT,
+        Mat3>
+        KernelType;
+
+    KernelType kernel(xA, imgSizeA.first, imgSizeA.second,
+                      xB, imgSizeB.first, imgSizeB.second,
+                      m_K, m_K);
+
+    // Robustly estimate the Essential matrix with A Contrario ransac
+    Mat3 E;
+    double upper_bound_precision = m_dPrecision;
+    std::pair<double,double> ACRansacOut =
+      ACRANSAC(kernel, vec_inliers, m_stIteration, &E, upper_bound_precision);
+    const double & threshold = ACRansacOut.first;
+    const double & NFA = ACRansacOut.second;
+
+    if (vec_inliers.size() < KernelType::MINIMUM_SAMPLES *2.5)  {
+      vec_inliers.clear();
+    }
+  }
+
+  double m_dPrecision;  //upper_bound of the precision
+  size_t m_stIteration; //maximal number of used iterations
+  Mat3 m_K; // the considered intrinsic matrix
+};
