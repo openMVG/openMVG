69531ed1e09d56eb2cd61102d04d3345a764fac3
pmoulon <pmoulon@gmail.com>
2014-04-01 12:26:55 +0200

    Make variable local for multithread evaluation. #115

--- src/software/SfM/SfMReconstructionData.hpp
+++ src/software/SfM/SfMReconstructionData.hpp
@@ -90,12 +90,12 @@ Image undistortImage(
     offset = Vec2(cx,cy) - d.m_disto_center;
 
   Image J ( w,h );
-  double xu, yu, xd,yd;
 #ifdef USE_OPENMP
   #pragma omp parallel for
 #endif
   for (int j=0; j<h; j++ ) {
     for (int i=0; i<w; i++) {
+      double xu, yu, xd,yd;
       xu = double (i);
       yu = double (j);
       d.ComputeUndistortedCoordinates(xu, yu, xd, yd);
