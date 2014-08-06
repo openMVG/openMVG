7e71109a3ab898cbddf8bb493d316a926e3f01f8
pmoulon <pmoulon@gmail.com>
2014-08-06 23:59:30 +0200

    Add a pair_builder for contiguous image sequence (assuming a local overlap).

--- src/openMVG/matching_image_collection/Pair_Builder.hpp
+++ src/openMVG/matching_image_collection/Pair_Builder.hpp
@@ -10,7 +10,7 @@ namespace openMVG {
   
 typedef std::set<std::pair<size_t, size_t> > PairsT;
   
-// Generate all the (I,J) pairs of the upper diagonal of the NxN matrix  
+/// Generate all the (I,J) pairs of the upper diagonal of the NxN matrix
 PairsT exhaustivePairs(const size_t N)
 {
   PairsT pairs;
@@ -18,6 +18,19 @@ PairsT exhaustivePairs(const size_t N)
     for(size_t J = I+1; J < N; ++J)
       pairs.insert(std::make_pair(I,J));
 
+  return pairs;
+}
+
+/// Generate the pairs that have a distance inferior to the overlapSize
+/// Usable to match videos sequence
+PairsT contiguousWithOverlap(const size_t N, const size_t overlapSize)
+{
+  PairsT pairs;
+  for(int I = 0; I < N; ++I)
+    for(int J = I+1; J < I+1+overlapSize && J < N; ++J)
+    {
+      pairs.insert(std::make_pair(I,J));
+    }
   return pairs;
 }
 
