88e945a4cff4dc24ae2ef51a501782cbd64fac4d
pmoulon <pmoulon@gmail.com>
2014-09-08 16:12:17 +0200

    Make computation of the intrinsic id simpler. #174

--- src/software/SfM/SfMIOHelper.hpp
+++ src/software/SfM/SfMIOHelper.hpp
@@ -37,27 +37,15 @@ struct IntrinsicCameraInfo
   bool m_bKnownIntrinsic; // true if 11 or 6, else false
   std::string m_sCameraMaker, m_sCameraModel;
 
-  // a case-insensitive comparison function:
-  static bool mycomp (char c1, char c2)
-  {
-    if (std::isdigit(c1) || std::isdigit(c2))
-      return c1 < c2;
-    else
-      return std::tolower(c1) < std::tolower(c2);
-  }
+  IntrinsicCameraInfo(): m_w(0), m_h(0), m_K(Mat3::Zero()), m_bKnownIntrinsic(false), m_sCameraModel(""), m_sCameraMaker("")
+  {  }
 
-  /// Functor used for having a strict weak ordering of IntrinsicCameraInfo
-  /// return false (when elements are equivalent).
-  bool operator() (IntrinsicCameraInfo const &ci1, IntrinsicCameraInfo const &ci2)const
+  /// Functor used to tell if two IntrinsicCameraInfo share the same optical properties
+  friend bool operator== (IntrinsicCameraInfo const &ci1, IntrinsicCameraInfo const &ci2)
   {
-    return ( std::lexicographical_compare(
-        ci1.m_sCameraMaker.c_str(),ci1.m_sCameraMaker.c_str()+ci1.m_sCameraMaker.length(),
-        ci2.m_sCameraMaker.c_str(),ci2.m_sCameraMaker.c_str()+ci2.m_sCameraMaker.length(),mycomp) &&
-      std::lexicographical_compare(
-        ci1.m_sCameraModel.c_str(),ci1.m_sCameraModel.c_str()+ci1.m_sCameraModel.length(),
-        ci2.m_sCameraModel.c_str(),ci2.m_sCameraModel.c_str()+ci2.m_sCameraModel.length(),mycomp) )
-        ||
-      std::lexicographical_compare(ci1.m_K.data(),ci1.m_K.data()+9,ci2.m_K.data(),ci2.m_K.data()+9);
+    // Two camera share optical properties if they share the same K matrix (and the same camera name)
+    bool bequal = ci1.m_K == ci2.m_K && ci1.m_sCameraMaker == ci2.m_sCameraMaker && ci1.m_sCameraModel == ci2.m_sCameraModel;
+    return bequal;
   }
 };
 
@@ -73,9 +61,6 @@ static bool loadImageList( std::vector<CameraInfo> & vec_camImageName,
                            const std::string & sFileName,
                            bool bVerbose = true )
 {
-  typedef std::set<IntrinsicCameraInfo, IntrinsicCameraInfo> setIntrinsicCameraInfo;
-  setIntrinsicCameraInfo set_focalGroup;
-
   std::ifstream in(sFileName.c_str());
   if(!in.is_open())  {
     std::cerr << std::endl
@@ -86,7 +71,6 @@ static bool loadImageList( std::vector<CameraInfo> & vec_camImageName,
   while(getline( in, sValue ) )
   {
     vec_str.clear();
-    IntrinsicCameraInfo intrinsicCamInfo;
     split( sValue, ";", vec_str );
     if (vec_str.size() == 1)
     {
@@ -101,6 +85,7 @@ static bool loadImageList( std::vector<CameraInfo> & vec_camImageName,
     oss.clear(); oss.str(vec_str[2]);
     oss >> height;
 
+    IntrinsicCameraInfo intrinsicCamInfo;
     intrinsicCamInfo.m_w = width;
     intrinsicCamInfo.m_h = height;
 
@@ -137,7 +122,6 @@ static bool loadImageList( std::vector<CameraInfo> & vec_camImageName,
               0, focal, float(height) / 2.f,
               0, 0, 1;
          intrinsicCamInfo.m_K = K;
-
       }
       break;
       case 12 : // a camera with known intrinsic
@@ -178,12 +162,18 @@ static bool loadImageList( std::vector<CameraInfo> & vec_camImageName,
       }
     }
 
-    std::pair<setIntrinsicCameraInfo::iterator, bool> ret = set_focalGroup.insert(intrinsicCamInfo);
-    if ( ret.second )
+    std::vector<IntrinsicCameraInfo>::const_iterator iterIntrinsicGroup = find(vec_focalGroup.begin(), vec_focalGroup.end(), intrinsicCamInfo);
+    size_t id = -1;
+    if ( iterIntrinsicGroup == vec_focalGroup.end())
     {
       vec_focalGroup.push_back(intrinsicCamInfo);
+      id = vec_focalGroup.size()-1;
     }
-    size_t id = std::distance( ret.first, set_focalGroup.end()) - 1;
+    else
+    {
+      id = std::distance( std::vector<IntrinsicCameraInfo>::const_iterator(vec_focalGroup.begin()), iterIntrinsicGroup);
+    }
+    
     CameraInfo camInfo;
     camInfo.m_sImageName = vec_str[0];
     camInfo.m_intrinsicId = id;
