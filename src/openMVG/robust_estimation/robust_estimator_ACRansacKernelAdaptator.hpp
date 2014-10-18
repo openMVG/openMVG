c704f20634cd328240179d5b3d9d4f065ce9b51a
pmoulon <pmoulon@gmail.com>
2014-10-18 17:22:32 +0200

    Add a new sample. AContrario essential matrix estimation between two spherical panorama (Based on angular residual errors). #193

--- src/openMVG/robust_estimation/robust_estimator_ACRansacKernelAdaptator.hpp
+++ src/openMVG/robust_estimation/robust_estimator_ACRansacKernelAdaptator.hpp
@@ -9,14 +9,19 @@
 #define OPENMVG_ROBUST_ESTIMATOR_ACRANSAC_KERNEL_ADAPTATOR_H_
 
 // Here a collection of A contrario Kernel adaptor.
+//  - See // [1] "Robust and accurate calibration of camera networks". PhD.
+//  - Authors: Pierre MOULON
+//
 //ACKernelAdaptor
-//  For general two view estimation (affine, homography, fundamental)
+//  i.e. general two view estimation (affine, homography, fundamental)
 //ACKernelAdaptorResection
 //  For pose/resection estimation
 //ACKernelAdaptorResection_K
 //  For pose/resection with known camera intrinsic
 //ACKernelAdaptorEssential
 //  For essential matrix estimation
+//ACKernelAdaptor_AngularRadianError
+//  i.e. essential matrix estimation between spherical camera
 //
 // Mainly it add correct data normalization and define the function required
 //  by the generic ACRANSAC routine.
@@ -41,8 +46,9 @@ public:
   typedef ModelArg  Model;
   typedef ErrorArg ErrorT;
 
-  ACKernelAdaptor(const Mat &x1, int w1, int h1,
-             const Mat &x2, int w2, int h2, bool bPointToLine = true)
+  ACKernelAdaptor(
+    const Mat &x1, int w1, int h1,
+    const Mat &x2, int w2, int h2, bool bPointToLine = true)
     : x1_(x1.rows(), x1.cols()), x2_(x2.rows(), x2.cols()),
     N1_(3,3), N2_(3,3), logalpha0_(0.0), bPointToLine_(bPointToLine)
   {
@@ -284,6 +290,64 @@ private:
   Mat3 N1_, N2_;      // Matrix used to normalize data
   double logalpha0_; // Alpha0 is used to make the error adaptive to the image size
   Mat3 K1_, K2_;      // Intrinsic camera parameter
+};
+
+/// Two view Kernel adaptator for the A contrario model estimator.
+/// Specialization to handle radian angular residual error.
+template <typename SolverArg,
+          typename ErrorArg,
+          typename UnnormalizerArg,
+          typename ModelArg = Mat3>
+class ACKernelAdaptor_AngularRadianError
+{
+public:
+  typedef SolverArg Solver;
+  typedef ModelArg  Model;
+  typedef ErrorArg ErrorT;
+
+  ACKernelAdaptor_AngularRadianError(
+    const Mat & xA,
+    const Mat & xB):
+    x1_(xA), x2_(xB),
+    logalpha0_(log10(1.0/2.0))
+  {
+    assert(3 == x1_.rows());
+    assert(x1_.rows() == x2_.rows());
+    assert(x1_.cols() == x2_.cols());
+  }
+
+  enum { MINIMUM_SAMPLES = Solver::MINIMUM_SAMPLES };
+  enum { MAX_MODELS = Solver::MAX_MODELS };
+
+  void Fit(const std::vector<size_t> &samples, std::vector<Model> *models) const {
+    const Mat x1 = ExtractColumns(x1_, samples);
+    const Mat x2 = ExtractColumns(x2_, samples);
+    Solver::Solve(x1, x2, models);
+  }
+
+  double Error(size_t sample, const Model &model) const {
+    return Square(ErrorT::Error(model, x1_.col(sample), x2_.col(sample)));
+  }
+
+  size_t NumSamples() const {
+    return static_cast<size_t>(x1_.cols());
+  }
+
+  void Unnormalize(Model * model) const {
+    //-- Do nothing, no normalization in the angular case
+  }
+
+  double logalpha0() const {return logalpha0_;}
+
+  double multError() const {return 1./4.;}
+
+  Mat3 normalizer1() const {return Mat3::Identity();}
+  Mat3 normalizer2() const {return Mat3::Identity();}
+  double unormalizeError(double val) const {return sqrt(val);}
+
+private:
+  Mat x1_, x2_;       // Normalized input data
+  double logalpha0_; // Alpha0 is used to make the error scale invariant
 };
 
 } // namespace robust
