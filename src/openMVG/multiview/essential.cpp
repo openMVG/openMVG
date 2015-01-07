00e944e0bbd7a1984d7f0a701f081c39321b609f
pmoulon <pmoulon@gmail.com>
2015-01-07 18:03:32 +0100

    Fix some typo.

--- src/openMVG/multiview/essential.cpp
+++ src/openMVG/multiview/essential.cpp
@@ -32,7 +32,7 @@
 
 namespace openMVG {
 
-// HZ 9.6 pag 257 (formula 9.12)
+// HZ 9.6 page 257 (formula 9.12)
 void EssentialFromFundamental(const Mat3 &F,
                               const Mat3 &K1,
                               const Mat3 &K2,
@@ -40,7 +40,7 @@ void EssentialFromFundamental(const Mat3 &F,
   *E = K2.transpose() * F * K1;
 }
 
-// HZ 9.6 pag 257 (formula 9.12)
+// HZ 9.6 page 257 (formula 9.12)
 // Or http://ai.stanford.edu/~birch/projective/node20.html
 void FundamentalFromEssential(const Mat3 &E,
                               const Mat3 &K1,
@@ -59,7 +59,7 @@ void RelativeCameraMotion(const Mat3 &R1,
   *t = t2 - (*R) * t1;
 }
 
-// HZ 9.6 pag 257
+// HZ 9.6 page 257
 void EssentialFromRt(const Mat3 &R1,
                      const Vec3 &t1,
                      const Mat3 &R2,
