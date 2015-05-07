ea0c5d3495c00379e9216702775f315b7cf3a918
pmoulon <pmoulon@gmail.com>
2015-05-07 15:09:32 +0200

    remove unused include

--- src/software/SfM/main_ComputeMatches.cpp
+++ src/software/SfM/main_ComputeMatches.cpp
@@ -24,7 +24,6 @@
 
 #include "third_party/cmdLine/cmdLine.h"
 #include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
-#include "third_party/progress/progress.hpp"
 
 #include <cstdlib>
 #include <fstream>
