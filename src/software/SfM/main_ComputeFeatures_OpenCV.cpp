7c5a01309c384400640f85ec9e3edf090f575d5c
Clement Aymard <blog.clm@gmail.com>
2015-06-17 19:15:12 +0200

    add the OpenCV SIFT feature as an option for ComputeFeature_OpenCV

--- src/software/SfM/main_ComputeFeatures_OpenCV.cpp
+++ src/software/SfM/main_ComputeFeatures_OpenCV.cpp
@@ -22,6 +22,9 @@
 /// OpenCV Includes
 #include <opencv2/opencv.hpp>
 #include "opencv2/core/eigen.hpp"
+#ifdef USE_OCVSIFT
+#include "opencv2/xfeatures2d.hpp"
+#endif
 
 #include <cstdlib>
 #include <fstream>
@@ -129,6 +132,89 @@ public:
 #include <cereal/archives/json.hpp>
 CEREAL_REGISTER_TYPE_WITH_NAME(AKAZE_OCV_Image_describer, "AKAZE_OCV_Image_describer");
 
+#ifdef USE_OCVSIFT
+///
+//- Create an Image_describer interface that use and OpenCV extraction method
+// i.e. with the SIFT detector+descriptor
+// Regions is the same as classic SIFT : 128 unsigned char
+class SIFT_OPENCV_Image_describer : public Image_describer
+{
+public:
+  SIFT_OPENCV_Image_describer() : Image_describer() {}
+
+  ~SIFT_OPENCV_Image_describer() {}
+
+  bool Set_configuration_preset(EDESCRIBER_PRESET preset){
+    return true;
+  }
+
+  /**
+  @brief Detect regions on the image and compute their attributes (description)
+  @param image Image.
+  @param regions The detected regions and attributes (the caller must delete the allocated data)
+  @param mask 8-bit gray image for keypoint filtering (optional).
+     Non-zero values depict the region of interest.
+  */
+  bool Describe(const image::Image<unsigned char>& image,
+    std::unique_ptr<Regions> &regions,
+    const image::Image<unsigned char> * mask = NULL)
+  {
+    // Convert for opencv
+    cv::Mat img;
+    cv::eigen2cv(image.GetMat(), img);
+
+    // Create a SIFT detector
+    std::vector< cv::KeyPoint > v_keypoints;
+    cv::Mat m_desc;
+    cv::Ptr<cv::Feature2D> siftdetector = cv::xfeatures2d::SIFT::create();
+
+    // Process SIFT computation
+    siftdetector->detectAndCompute(img, cv::Mat(), v_keypoints, m_desc);
+
+    Allocate(regions);
+
+    // Build alias to cached data
+    SIFT_Regions * regionsCasted = dynamic_cast<SIFT_Regions*>(regions.get());
+    // reserve some memory for faster keypoint saving
+    regionsCasted->Features().reserve(v_keypoints.size());
+    regionsCasted->Descriptors().reserve(v_keypoints.size());
+
+    // Copy keypoints and descriptors in the regions
+    int cpt = 0;
+    for(std::vector< cv::KeyPoint >::const_iterator i_kp = v_keypoints.begin();
+        i_kp != v_keypoints.end();
+        ++i_kp, ++cpt)
+    {
+      SIOPointFeature feat((*i_kp).pt.x, (*i_kp).pt.y, (*i_kp).size, (*i_kp).angle);
+      regionsCasted->Features().push_back(feat);
+
+      Descriptor<unsigned char, 128> desc;
+      for(int j = 0; j < 128; j++)
+      {
+        desc[j] = static_cast<unsigned char>(m_desc.at<float>(cpt, j));
+      }
+      regionsCasted->Descriptors().push_back(desc);
+    }
+
+    return true;
+  };
+
+  /// Allocate Regions type depending of the Image_describer
+  void Allocate(std::unique_ptr<Regions> &regions) const
+  {
+    regions.reset( new SIFT_Regions );
+  }
+
+  template<class Archive>
+  void serialize( Archive & ar )
+  {
+  }
+private:
+  int _i;
+};
+CEREAL_REGISTER_TYPE_WITH_NAME(SIFT_OPENCV_Image_describer, "SIFT_OPENCV_Image_describer");
+#endif //USE_OCVSIFT
+
 /// Compute between the Views
 /// Compute view image description (feature & descriptor extraction using OpenCV)
 /// Compute putative local feature matches (descriptor matching)
@@ -141,12 +227,18 @@ int main(int argc, char **argv)
   std::string sSfM_Data_Filename;
   std::string sOutDir = "";
   bool bForce = false;
+#ifdef USE_OCVSIFT
+  std::string sImage_Describer_Method = "AKAZE_OCV";
+#endif
 
   // required
   cmd.add( make_option('i', sSfM_Data_Filename, "input_file") );
   cmd.add( make_option('o', sOutDir, "outdir") );
   // Optional
   cmd.add( make_option('f', bForce, "force") );
+#ifdef USE_OCVSIFT
+  cmd.add( make_option('m', sImage_Describer_Method, "describerMethod") );
+#endif
 
   try {
       if (argc == 1) throw std::string("Invalid command line parameter.");
@@ -157,6 +249,12 @@ int main(int argc, char **argv)
       << "[-o|--outdir path] \n"
       << "\n[Optional]\n"
       << "[-f|--force: Force to recompute data]\n"
+#ifdef USE_OCVSIFT
+	  << "[-m|--describerMethod\n"
+      << "  (method to use to describe an image):\n"
+      << "   AKAZE_OPENCV (default),\n"
+      << "   SIFT_OPENCV: SIFT FROM OPENCV,\n"
+#endif
       << std::endl;
 
       std::cerr << s << std::endl;
@@ -166,7 +264,11 @@ int main(int argc, char **argv)
   std::cout << " You called : " <<std::endl
             << argv[0] << std::endl
             << "--input_file " << sSfM_Data_Filename << std::endl
-            << "--outdir " << sOutDir << std::endl;
+            << "--outdir " << sOutDir << std::endl
+#ifdef USE_OCVSIFT
+            << "--describerMethod " << sImage_Describer_Method << std::endl
+#endif
+		    ;
 
   if (sOutDir.empty())  {
     std::cerr << "\nIt is an invalid output directory" << std::endl;
@@ -213,8 +315,25 @@ int main(int argc, char **argv)
   }
   else
   {
+#ifdef USE_OCVSIFT
+	if (sImage_Describer_Method == "AKAZE_OPENCV")
+    {
+		image_describer.reset(new AKAZE_OCV_Image_describer);
+    }
+    else
+    if (sImage_Describer_Method == "SIFT_OPENCV")
+    {
+      image_describer.reset(new SIFT_OPENCV_Image_describer());
+    }
+    else
+    {
+		std::cerr << "Unknown image describer method." << std::endl;
+		return EXIT_FAILURE;
+    }
+#else
     image_describer.reset(new AKAZE_OCV_Image_describer);
-
+#endif
+	
     // Export the used Image_describer and region type for:
     // - dynamic future regions computation and/or loading
     {
