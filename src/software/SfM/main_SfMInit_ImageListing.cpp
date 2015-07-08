8db4043edb676cadb4a9eb020aff442b15df8176
Elisa Prana <pra@mikrosimage.eu>
2015-07-08 16:51:18 +0200

    Add option in SfmInit_ImageListing.cpp to load images from json file
    * Add an option (-j) to load image list from a json file
    It enables to load files and directories
    
    Conflicts:
    
    	src/software/SfM/main_SfMInit_ImageListing.cpp

--- src/software/SfM/main_SfMInit_ImageListing.cpp
+++ src/software/SfM/main_SfMInit_ImageListing.cpp
@@ -54,6 +54,83 @@ bool checkIntrinsicStringValidity(const std::string & Kmatrix, double & focal, d
   return true;
 }
 
+/// Recursively list all files from a folder with a specific extension
+void listFiles(const std::string& folderOrFile, const std::vector<std::string>& extensions, std::vector<std::string>& outFiles)
+{
+  if(stlplus::is_file(folderOrFile))
+  {
+    std::string fileExtension = stlplus::extension_part(folderOrFile);
+    std::transform(fileExtension.begin(), fileExtension.end(), fileExtension.begin(), ::tolower);
+    for(const std::string& extension: extensions)
+    {
+      if(fileExtension == extension)
+      {
+        outFiles.push_back(folderOrFile);
+        return;
+      }
+    }
+  }
+  else if(stlplus::is_folder(folderOrFile))
+  {
+    // list all files of the folder
+    const std::vector<std::string> allFiles = stlplus::folder_all(folderOrFile);
+    for(const std::string& item: allFiles)
+    {
+      const std::string itemPath = stlplus::create_filespec(folderOrFile, item);
+      listFiles(itemPath, extensions, outFiles);
+    }
+  }
+}
+
+/// Retrieve resources path from a json file
+/// Need a "resource" variable in the json
+bool retrieveResources(const std::string& jsonFile, std::vector<std::string>& vec_imagePaths)
+{
+  if(!stlplus::file_exists(jsonFile))
+  {
+    std::cerr << "File \"" << jsonFile << "\" does not exists." << std::endl;
+    return false;
+  }
+
+  // Read file
+  std::ifstream jsonStream(jsonFile.c_str(), std::ifstream::binary);
+  if(jsonStream)
+  {
+    // get length of file:
+    jsonStream.seekg (0, jsonStream.end);
+    const int length = jsonStream.tellg();
+    jsonStream.seekg (0, jsonStream.beg);
+    // read data as a block:
+    std::string jsonString;
+    jsonString.resize(length);
+    jsonStream.read(&jsonString[0], length);
+    jsonStream.close();
+    if(!jsonStream)
+    {
+      std::cerr << "Error when reading file." << std::endl;
+      return false;
+    }
+    // Parse json
+    rapidjson::Document document;
+    document.Parse<0>(&jsonString[0]);
+    if(!document.IsObject())
+    {
+      std::cerr << "File \"" << jsonFile << "\" is not in json format." << std::endl;
+      return false;
+    }
+    if(!document.HasMember("resources"))
+    {
+      std::cerr << "No member \"resources\" in json file" << std::endl;
+      return false;
+    }
+    rapidjson::Value& resourcesValue = document["resources"];
+    for(rapidjson::Value::ConstValueIterator it = resourcesValue.Begin(); it != resourcesValue.End(); it++)
+      vec_imagePaths.push_back(it->GetString());
+
+    return true;
+  }
+}
+
 //
 // Create the description of an input image dataset for OpenMVG toolsuite
 // - Export a SfM_Data file with View & Intrinsic data
@@ -62,10 +139,11 @@ int main(int argc, char **argv)
 {
   CmdLine cmd;
 
-  std::string sImageDir,
-    sfileDatabase = "",
-    sOutputDir = "",
-    sKmatrix;
+  std::string sImageDir;
+  std::string sJsonFile;
+  std::string sfileDatabase;
+  std::string sOutputDir;
+  std::string sKmatrix;
 
   int i_User_camera_model = PINHOLE_CAMERA_RADIAL3;
 
@@ -75,6 +153,7 @@ int main(int argc, char **argv)
   double focal_pixels = -1.0;
 
   cmd.add( make_option('i', sImageDir, "imageDirectory") );
+  cmd.add( make_option('j', sJsonFile, "jsonFile") );
   cmd.add( make_option('d', sfileDatabase, "sensorWidthDatabase") );
   cmd.add( make_option('o', sOutputDir, "outputDirectory") );
   cmd.add( make_option('f', focal_pixels, "focal") );
@@ -86,9 +165,11 @@ int main(int argc, char **argv)
   try {
       if (argc == 1) throw std::string("Invalid command line parameter.");
       cmd.process(argc, argv);
-  } catch(const std::string& s) {
+  } catch(const std::string& s)
+  {
       std::cerr << "Usage: " << argv[0] << '\n'
       << "[-i|--imageDirectory]\n"
+      << "[-j|--jsonFile]\n"
       << "[-d|--sensorWidthDatabase]\n"
       << "[-o|--outputDirectory]\n"
       << "[-f|--focal] (pixels)\n"
@@ -111,6 +192,7 @@ int main(int argc, char **argv)
   std::cout << " You called : " <<std::endl
             << argv[0] << std::endl
             << "--imageDirectory " << sImageDir << std::endl
+            << "--jsonFile " << sJsonFile << std::endl
             << "--sensorWidthDatabase " << sfileDatabase << std::endl
             << "--outputDirectory " << sOutputDir << std::endl
             << "--focal " << focal_pixels << std::endl
@@ -123,12 +205,16 @@ int main(int argc, char **argv)
 
   const EINTRINSIC e_User_camera_model = EINTRINSIC(i_User_camera_model);
 
-  if ( !stlplus::folder_exists( sImageDir ) )
+  if(!sImageDir.empty() && !sJsonFile.empty())
+  {
+    std::cerr << "\nCannot combine -i and -j options" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if ( !sImageDir.empty() && !stlplus::folder_exists( sImageDir ) )
   {
     std::cerr << "\nThe input directory doesn't exist" << std::endl;
     return EXIT_FAILURE;
   }
-
   if (sOutputDir.empty())
   {
     std::cerr << "\nInvalid output directory" << std::endl;
@@ -169,27 +255,53 @@ int main(int argc, char **argv)
     }
   }
 
-  std::vector<std::string> vec_image = stlplus::folder_files( sImageDir );
-  std::sort(vec_image.begin(), vec_image.end());
+  // Retrieve image paths
+  std::vector<std::string> vec_images;
+  const std::vector<std::string> supportedExtensions{ "jpg", "jpeg" };
+
+  if (!sJsonFile.empty())
+  {
+    // Retrieve resources from json file
+    std::vector<std::string> vec_resources;
+    if (!retrieveResources(sJsonFile, vec_resources))
+      return EXIT_FAILURE;
+    // Retrieve images from resources
+    for(const std::string& item: vec_resources)
+    {
+      listFiles(item, supportedExtensions, vec_images);
+    }
+    std::sort(vec_images.begin(), vec_images.end());
+  }
+  if(!sImageDir.empty())
+  {
+    vec_images = stlplus::folder_files( sImageDir );
+  }
+  std::sort(vec_images.begin(), vec_images.end());
 
   // Configure an empty scene with Views and their corresponding cameras
   SfM_Data sfm_data;
-  sfm_data.s_root_path = sImageDir; // Setup main image root_path
+  sfm_data.s_root_path = "";
+  if(!sImageDir.empty())
+    sfm_data.s_root_path = sImageDir; // Setup main image root_path
   Views & views = sfm_data.views;
   Intrinsics & intrinsics = sfm_data.intrinsics;
 
-  C_Progress_display my_progress_bar( vec_image.size(),
+  C_Progress_display my_progress_bar( vec_images.size(),
       std::cout, "\n- Image listing -\n" );
   std::ostringstream error_report_stream;
-  for ( std::vector<std::string>::const_iterator iter_image = vec_image.begin();
-    iter_image != vec_image.end();
+  for ( std::vector<std::string>::const_iterator iter_image = vec_images.begin();
+    iter_image != vec_images.end();
     ++iter_image, ++my_progress_bar )
   {
     // Read meta data to fill camera parameter (w,h,focal,ppx,ppy) fields.
     width = height = ppx = ppy = focal = -1.0;
-    
+
     const std::string imageFilename = *iter_image;
-    const std::string imageAbsFilepath = stlplus::create_filespec( sImageDir, imageFilename );
+    std::string imageAbsFilepath;
+    if(!sJsonFile.empty())
+      imageAbsFilepath = imageFilename;
+    else if(!sImageDir.empty())
+      imageAbsFilepath = stlplus::create_filespec( sImageDir, imageFilename );
 
     // Test if the image format is supported:
     if (openMVG::image::GetFormat(imageAbsFilepath.c_str()) == openMVG::image::Unknown)
@@ -342,7 +454,7 @@ int main(int argc, char **argv)
 
   std::cout << std::endl
     << "SfMInit_ImageListing report:\n"
-    << "listed #File(s): " << vec_image.size() << "\n"
+    << "listed #File(s): " << vec_images.size() << "\n"
     << "usable #File(s) listed in sfm_data: " << sfm_data.GetViews().size() << std::endl;
 
   return EXIT_SUCCESS;
