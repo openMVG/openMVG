79579d643746e1b80bfffbceb192759dee0de0ff
pmoulon <pmoulon@gmail.com>
2015-08-12 17:00:29 +0200

    Add licence headers.

--- src/openMVG/exif/exif_IO_test.cpp
+++ src/openMVG/exif/exif_IO_test.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2013-2015 Pierre MOULON.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include "exif_IO_EasyExif.hpp"
 
 #include "testing/testing.h"
