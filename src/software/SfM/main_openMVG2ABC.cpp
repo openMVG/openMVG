8287112df59a0e37638bc27539121549c26b006e
Cyril Pichard <pih@mikrosimage.eu>
2015-09-11 18:43:07 +0200

    export colored alembic point cloud

--- src/software/SfM/main_openMVG2ABC.cpp
+++ src/software/SfM/main_openMVG2ABC.cpp
@@ -15,6 +15,8 @@
 #include "openMVG/numeric/numeric.h"
 #include "openMVG/geometry/pose3.hpp"
 using namespace openMVG::sfm;
+using namespace openMVG::image;
+using openMVG::IndexT;
 
 // Alembic
 #include <Alembic/AbcGeom/All.h>
@@ -22,6 +24,89 @@ using namespace openMVG::sfm;
 using namespace Alembic::Abc;
 namespace AbcG = Alembic::AbcGeom;
 using namespace AbcG;
+//
+// The following function is taken from main_ComputeSfM_DataColor and slightly modified
+//
+void findTracksColors( const SfM_Data & sfm_data, 
+                       const std::vector<IndexT> & trackIds,
+                        std::vector<Imath::C3f> & colors)
+{
+    // Colorize each track
+    //  Start with the most representative image
+    //    and iterate to provide a color to each 3D point
+    C_Progress_display my_progress_bar(sfm_data.GetLandmarks().size(),
+                                       std::cout,
+                                       "\nCompute scene structure color\n");
+
+    // The track list indexes that will be colored 
+    // (point removed during the process)
+    std::set<IndexT> remainingTrackToColor;
+    for(int i=0; i < trackIds.size(); i++)
+    {
+        remainingTrackToColor.insert(remainingTrackToColor.end(), i);
+    }
+    // Number of observation per view
+    const size_t nViews = sfm_data.GetViews().size();
+    std::vector<size_t> obsPerView(nViews);
+
+    while( !remainingTrackToColor.empty() )
+    {
+        // Find the most representative image (for the remaining 3D points)
+        //  a. Count the number of observation per view for each 3Dpoint Index
+        //  b. Find the View index that is the most represented ie has the biggest
+        //  number of observations
+        size_t view_index = 0; // FIXME not logical, could be "no view found"
+        size_t maxObs = 0;
+        std::fill(obsPerView.begin(), obsPerView.end(), 0);
+
+        for(auto iterT: remainingTrackToColor)
+        {
+            const size_t trackId = trackIds[iterT];
+            const Observations & obs = sfm_data.GetLandmarks().at(trackId).obs;
+            for( Observations::const_iterator iterObs = obs.begin();
+            iterObs != obs.end(); ++iterObs)
+            {
+                const size_t viewId = iterObs->first;
+                obsPerView[viewId]++;
+                if (obsPerView[viewId] >= maxObs )
+                {
+                    maxObs = obsPerView[viewId];
+                    view_index = viewId;
+                }
+            }
+        }
+
+        const View * view = sfm_data.GetViews().at(view_index).get();
+        const std::string sView_filename = stlplus::create_filespec(sfm_data.s_root_path, view->s_Img_path);
+        Image<RGBColor> image;
+        ReadImage(sView_filename.c_str(), &image);
+
+        // Iterate through the remaining track to color
+        // - look if the current view is present to color the track
+        std::set<IndexT> trackProcessed;
+        for(auto iterT: remainingTrackToColor)
+        {
+            const size_t trackId = trackIds[iterT];
+            const Observations & obs = sfm_data.GetLandmarks().at(trackId).obs;
+            Observations::const_iterator it = obs.find(view_index);
+
+            if(it != obs.end())
+            {
+                // Color the track
+                const openMVG::Vec2 & pt = it->second.x;
+                const RGBColor color = image(pt.y(), pt.x());
+                colors[iterT] = Imath::C3f(color.r()/255.f, color.g()/255.f, color.b()/255.f);
+                trackProcessed.insert(iterT);
+                ++my_progress_bar;
+            }
+        }
+
+        for(auto track: trackProcessed)
+        {
+            remainingTrackToColor.erase(track);
+        }
+    }
+}
 
 int main(int argc, char **argv) {
    
@@ -65,21 +150,35 @@ int main(int argc, char **argv) {
 
     std::vector<V3f> positions;
     positions.reserve(sfm_data.GetLandmarks().size());
+    
+    std::vector<IndexT> trackIds;
+    trackIds.reserve(sfm_data.GetLandmarks().size());
 
     // For all the 3d points in the hash_map
     for(const auto lm : sfm_data.GetLandmarks()) {
         const openMVG::Vec3 &pt = lm.second.X;
         positions.emplace_back(pt[0], pt[1], pt[2]);
+        trackIds.emplace_back(lm.first);
     }
+
+    std::vector<Imath::C3f> colors;
+    colors.resize(trackIds.size()); // R G B, 3 floats for the moment 
+    findTracksColors(sfm_data, trackIds, colors);
 
     std::vector<Alembic::Util::uint64_t> ids(positions.size());
     std::iota(std::begin(ids), std::end(ids), 0);
 
+    OPointsSchema::Sample psamp( std::move(V3fArraySample (positions)), std::move(UInt64ArraySample ( ids )));
     OPoints partsOut(topObj, "particleShape1");
     OPointsSchema &pSchema = partsOut.getSchema();
-    OPointsSchema::Sample psamp( std::move(V3fArraySample (positions)), std::move(UInt64ArraySample ( ids )));
     pSchema.set( psamp );
 
+    C3fArraySample val_samp( &colors[0], colors.size() );
+    OC3fGeomParam::Sample color_samp(val_samp, kVertexScope);
+    OCompoundProperty arbGeom = pSchema.getArbGeomParams();
+    OC3fGeomParam rgbOut(arbGeom, "color", false, kVertexScope, 1);
+    rgbOut.set(color_samp);
+    
     //=================================================================================
     // Export cameras
     //=================================================================================
@@ -95,16 +194,11 @@ int main(int argc, char **argv) {
         openMVG::Mat34 P = cam->get_projective_equivalent(pose);
 
         // Extract internal matrix, rotation and translation
-        //openMVG::Mat3 R, K;
-        //openMVG::Vec3 t;
-        //openMVG::KRt_From_P(P, &K, &R, &t);
-       
         const openMVG::Mat3 R = pose.rotation();
         const openMVG::Vec3 center = pose.center();
 
         // POSE
         // Compensate translation with rotation
-        //openMVG::Vec3 center = R.transpose() * t;
         // Build transform matrix
         Abc::M44d xformMatrix;
         xformMatrix[0][0]= R(0,0);
