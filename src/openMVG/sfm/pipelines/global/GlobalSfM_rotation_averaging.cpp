ff033e6c980608d58b54d478a5db7461df727836
Simone Gasparini <simone.gasparini@gmail.com>
2015-09-22 21:48:26 +0200

    [sfm] fixed missing switch case

--- src/openMVG/sfm/pipelines/global/GlobalSfM_rotation_averaging.cpp
+++ src/openMVG/sfm/pipelines/global/GlobalSfM_rotation_averaging.cpp
@@ -53,9 +53,12 @@ bool GlobalSfM_Rotation_AveragingSolver::Run(
         return false;
       KeepOnlyReferencedElement(set_remainingIds, relativeRotations);
     }
-    break;
+	break;
+    default:
+    std::cerr << "Unknown relative rotation inference method: " 
+            << (int) eRelativeRotationInferenceMethod << std::endl;
   }
-
+      
   // Compute contiguous index (mapping between sparse index and contiguous index)
   //  from ranging in [min(Id), max(Id)] to  [0, nbCam]
 
