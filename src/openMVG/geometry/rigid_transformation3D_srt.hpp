26781ed78d83028347eacaf8221c65310e0ccfba
Simone Gasparini <simone.gasparini@gmail.com>
2015-12-08 11:21:09 +0100

    [geometry] Added a robust version of FindRTS using ACRansac
    poparteu/openMVG#81

--- src/openMVG/geometry/rigid_transformation3D_srt.hpp
+++ src/openMVG/geometry/rigid_transformation3D_srt.hpp
@@ -10,8 +10,51 @@
 #include "openMVG/numeric/numeric.h"
 #include "openMVG/numeric/lm.hpp"
 
+#include <openMVG/robust_estimation/robust_estimator_ACRansac.hpp>
+
 namespace openMVG {
 namespace geometry {
+
+/**
+ * @brief Compose a similarity matrix given a scale factor, a rotation matrix and 
+ * a translation vector.
+ * 
+ * @param[in] S The scale factor.
+ * @param[in] t The translation vector.
+ * @param[in] R The rotation matrix.
+ * @param[out] RTS The 4x4 similarity matrix [S*R | t; 0 0 0 1].
+ */
+static void composeRTS(double &S, const Vec3 &t, const Mat3 &R, Mat4 &RTS)
+{
+  RTS.topLeftCorner(3, 3) = S*R;
+  RTS.topRightCorner(3, 1) = t;
+}
+
+/**
+ * @brief Decompose a similarity matrix into its scale, rotation and translation parts
+ * 
+ * @param[in] RTS The similarity matrix to decompose.
+ * @param[out] S The scale factor.
+ * @param[out] t The translation part.
+ * @param[out] R The rotation part.
+ * @return true if the input matrix is a similarity matrix.
+ */
+static bool decomposeRTS(const Mat4 &RTS, double &S, Vec3 &t, Mat3 &R)
+{
+  // Check critical cases
+  R = RTS.topLeftCorner<3, 3>();
+  if (R.determinant() < 0)
+    return false;
+  S = pow(R.determinant(), 1.0 / 3.0);
+  // Check for degenerate case (if all points have the same value...)
+  if (S < std::numeric_limits<double>::epsilon())
+    return false;
+
+  // Extract transformation parameters
+  R /= S;
+  t = RTS.topRightCorner<3, 1>();
+  return true;
+}
 
 /** 3D rigid transformation estimation (7 dof)
  * Compute a Scale Rotation and Translation rigid transformation.
@@ -52,21 +95,7 @@ static bool FindRTS(const Mat &x1,
   // from which we can extract the scale, rotation, and translation.
   const Eigen::Matrix4d transform = Eigen::umeyama(x1, x2, true);
 
-  // Check critical cases
-  *R = transform.topLeftCorner<3, 3>();
-  if (R->determinant() < 0)
-    return false;
-  *S = pow(R->determinant(), 1.0 / 3.0);
-  // Check for degenerate case (if all points have the same value...)
-  if (*S < std::numeric_limits<double>::epsilon())
-    return false;
-
-  // Extract transformation parameters
-  *S = pow(R->determinant(), 1.0 / 3.0);
-  *R /= *S;
-  *t = transform.topRightCorner<3, 1>();
-
-  return true;
+  return decomposeRTS(transform, *S, *t, *R);
 }
 
 // Eigen LM functor to refine translation, Rotation and Scale parameter.
@@ -221,6 +250,217 @@ static void Refine_RTS(const Mat &x1,
 
     *R = (*R)*Rcor;
   }
+}
+
+/**
+ * @brief the Solver to use for ACRansac
+ */
+struct RTSSolver
+{
+
+  enum
+  {
+    MINIMUM_SAMPLES = 3
+  };
+
+  enum
+  {
+    MAX_MODELS = 1
+  };
+  
+  // Solve the RTS problem
+  static void Solve(const Mat &pts1, const Mat &pts2, std::vector<Mat4> *models)
+  {
+    models->push_back(Eigen::umeyama(pts1, pts2, true));
+  }
+
+  // Compute the residual of the transformation 
+  static double Error(const Mat4 &RTS, const Vec3 & pt1, const Vec3 & pt2)
+  {
+    const Mat3 &RS = RTS.topLeftCorner<3, 3>();
+    const Vec3 &t = RTS.topRightCorner<3, 1>();
+    return (pt2 - (RS*pt1 + t)).norm();
+  }
+};
+
+/**
+ * @brief A functor that computes the squared error between points transformed by
+ * a similarity
+ */
+struct RTSSquaredResidualError 
+{
+  // Return the squared error
+  static double Error(const Mat4 &RTS, const Vec3 & pt1, const Vec3 & pt2)
+  {
+    const Mat3 &RS = RTS.topLeftCorner<3, 3>();
+    const Vec3 &t = RTS.topRightCorner<3, 1>();
+    return (pt2 - (RS*pt1 + t)).squaredNorm();
+  }
+};
+
+/**
+ * @brief The kernel to use for ACRansac
+ */
+template <typename SolverArg,
+typename ErrorArg,
+typename ModelArg = Mat4>
+class ACKernelAdaptor_PointsRegistrationSRT
+{
+public:
+  typedef SolverArg Solver;
+  typedef ModelArg Model;
+  typedef ErrorArg ErrorT;
+
+  ACKernelAdaptor_PointsRegistrationSRT(const Mat & xA,
+                                        const Mat & xB) :
+             x1_(xA), x2_(xB), logalpha0_(log10(M_PI)) //@todo  WTF?
+  {
+    assert(3 == x1_.rows());
+    assert(x1_.rows() == x2_.rows());
+    assert(x1_.cols() == x2_.cols());
+    
+    // @todo normalize points?
+  }
+
+  enum
+  {
+    MINIMUM_SAMPLES = Solver::MINIMUM_SAMPLES
+  };
+
+  enum
+  {
+    MAX_MODELS = Solver::MAX_MODELS
+  };
+
+  void Fit(const std::vector<size_t> &samples, std::vector<Model> *models) const
+  {
+    const Mat x1 = ExtractColumns(x1_, samples);
+    const Mat x2 = ExtractColumns(x2_, samples);
+    Solver::Solve(x1, x2, models);
+  }
+
+  double Error(size_t sample, const Model &model) const
+  {
+    return Square(ErrorT::Error(model, x1_.col(sample), x2_.col(sample)));
+  }
+
+  void Errors(const Model & model, std::vector<double> & vec_errors) const
+  {
+    vec_errors.resize(x1_.cols());
+    for(size_t sample = 0; sample < x1_.cols(); ++sample)
+      vec_errors[sample] = Square(ErrorT::Error(model, x1_.col(sample), x2_.col(sample)));
+  }
+
+  size_t NumSamples() const { return static_cast<size_t> (x1_.cols()); }
+
+  void Unnormalize(Model * model) const { } //-- Do nothing, no normalization 
+
+  double logalpha0() const { return logalpha0_; }
+
+  double multError() const { return 1.;}
+
+  Mat3 normalizer1() const { return Mat3::Identity(); }
+
+  Mat3 normalizer2() const { return Mat3::Identity(); }
+
+  double unormalizeError(double val) const { return sqrt(val); }
+
+private:
+  Mat x1_, x2_;       // Normalized input data
+  double logalpha0_;  // Alpha0 is used to make the error scale invariant
+};
+
+/**
+ * @brief Uses AC ransac to robustly estimate the similarity between two sets of points.
+ * 
+ * @param[in] x1 The first 3xN matrix of euclidean points.
+ * @param[in] x2 The second 3xN matrix of euclidean points.
+ * @param[out] S The scale factor.
+ * @param[out] t The 3x1 translation.
+ * @param[out] R The 3x3 rotation.
+ * @param[out] vec_inliers The vector containing the indices of inliers points.
+ * @param[in] refine Enable/Disable refining of the found transformation.
+ * @return true if the found transformation is a similarity
+ * @see FindRTS()
+ */
+static bool ACRansac_FindRTS(const Mat &x1,
+                             const Mat &x2,
+                             double &S, 
+                             Vec3 &t, 
+                             Mat3 &R, 
+                             std::vector<std::size_t> &vec_inliers,
+                             bool refine = false)
+{
+  assert(3 == x1.rows());
+  assert(3 <= x1.cols());
+  assert(x1.rows() == x2.rows());
+  assert(x1.cols() == x2.cols());
+  
+  const std::size_t numIterations = 1024;
+  const double dPrecision = std::numeric_limits<double>::infinity();
+  
+  Mat4 RTS;
+
+  typedef geometry::RTSSolver SolverType;
+  typedef ACKernelAdaptor_PointsRegistrationSRT<
+          SolverType,
+          geometry::RTSSquaredResidualError> KernelType;
+
+  KernelType kernel = KernelType(x1, x2);
+  // Robust estimation of the Projection matrix and its precision
+  const std::pair<double, double> ACRansacOut =
+          robust::ACRANSAC(kernel, vec_inliers, numIterations, &RTS, dPrecision, true);
+  
+  const bool good = decomposeRTS(RTS, S, t, R);
+  
+  // return if it is not good or refine is not required
+  if(!good || !refine)
+    return good;
+
+  const std::size_t nbInliers = vec_inliers.size();
+  //only refine the inliers
+  Mat inliers1 = Mat3X(3, nbInliers);
+  Mat inliers2 = Mat3X(3, nbInliers);
+
+  for(std::size_t i = 0; i < nbInliers; ++i)
+  {
+    inliers1.col(i) = x1.col(vec_inliers[i]);
+    inliers2.col(i) = x2.col(vec_inliers[i]);
+  }
+
+  geometry::Refine_RTS(inliers1, inliers2, &S, &t, &R);
+  
+  return good;
+
+}
+
+/**
+ * @brief Uses AC ransac to robustly estimate the similarity between two sets of 
+ * points. Just a wrapper that output the similarity in matrix form
+ * @param[in] x1 The first 3xN matrix of euclidean points.
+ * @param[in] x2 The second 3xN matrix of euclidean points.
+ * @param[out] RTS The 4x4 similarity matrix. 
+ * @param[out] vec_inliers The inliers used to estimate the similarity.
+ * @param  refine Enable/Disable refining of the found transformation.
+ * @return true if the found transformation is a similarity
+ * @see geometry::FindRTS()
+ * @see geometry::ACRansac_FindRTS()
+ */
+static bool ACRansac_FindRTS(const Mat &x1, 
+                             const Mat &x2, 
+                             Mat4 &RTS, 
+                             std::vector<std::size_t> &vec_inliers,
+                             bool refine = false)
+{
+  double S;
+  Vec3 t; 
+  Mat3 R; 
+  const bool good = ACRansac_FindRTS(x1, x2, S, t, R, vec_inliers, refine);
+  
+  if(good)
+    composeRTS(S, t, R, RTS);
+  
+  return good;
 }
 
 } // namespace geometry
--- src/openMVG/geometry/rigid_transformation3D_srt_test.cpp
+++ src/openMVG/geometry/rigid_transformation3D_srt_test.cpp
@@ -4,95 +4,139 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "openMVG/geometry/rigid_transformation3D_srt.hpp"
+#include "rigid_transformation3D_srt.hpp"
+
+#include "openMVG/robust_estimation/robust_estimator_ACRansac.hpp"
+
 #include "CppUnitLite/TestHarness.h"
+
 #include "testing/testing.h"
+
 #include <iostream>
 
 using namespace openMVG;
 using namespace openMVG::geometry;
 using namespace std;
 
-TEST(SRT_precision, Experiment_ScaleOnly) {
+TEST(SRT_precision, Experiment_ScaleOnly)
+{
 
-  int nbPoints = 10;
-  Mat x1 = Mat::Random(3,nbPoints);
+  const std::size_t nbPoints = 10;
+  Mat x1 = Mat::Random(3, nbPoints);
   Mat x2 = x1;
 
-  double scale = 2;
+  const double scale = 2;
   Mat3 rot = Mat3::Identity();
-  Vec3 t(0,0,0);
+  Vec3 t(0, 0, 0);
 
-  for (int i=0; i < nbPoints; ++i)
+  for(std::size_t i = 0; i < nbPoints; ++i)
   {
     Vec3 pt = x1.col(i);
     x2.col(i) = (scale * rot * pt + t);
   }
 
   // Compute the Similarity transform
-  double Sc;
+  double Sc = 0;
   Mat3 Rc;
   Vec3 tc;
   FindRTS(x1, x2, &Sc, &tc, &Rc);
-  Refine_RTS(x1,x2,&Sc,&tc,&Rc);
+  Refine_RTS(x1, x2, &Sc, &tc, &Rc);
 
   std::cout << "\n"
-    << "Scale " << Sc << "\n"
-    << "Rot \n" << Rc << "\n"
-    << "t " << tc.transpose();
+          << "Scale " << Sc << "\n"
+          << "Rot \n" << Rc << "\n"
+          << "t " << tc.transpose();
 }
 
-TEST(SRT_precision, Experiment_ScaleAndRot) {
+TEST(SRT_precision, Experiment_ScaleAndRot)
+{
 
-  int nbPoints = 10;
-  Mat x1 = Mat::Random(3,nbPoints);
+  const std::size_t nbPoints = 10;
+  Mat x1 = Mat::Random(3, nbPoints);
   Mat x2 = x1;
 
-  double scale = 2;
+  const double scale = 2;
   Mat3 rot = (Eigen::AngleAxis<double>(.2, Vec3::UnitX())
-      * Eigen::AngleAxis<double>(.3, Vec3::UnitY())
-      * Eigen::AngleAxis<double>(.6, Vec3::UnitZ())).toRotationMatrix();
-  Vec3 t(0,0,0);
+          * Eigen::AngleAxis<double>(.3, Vec3::UnitY())
+          * Eigen::AngleAxis<double>(.6, Vec3::UnitZ())).toRotationMatrix();
+  Vec3 t(0, 0, 0);
 
-  for (int i=0; i < nbPoints; ++i)
+  for(std::size_t i = 0; i < nbPoints; ++i)
   {
     Vec3 pt = x1.col(i);
     x2.col(i) = (scale * rot * pt + t);
   }
 
   // Compute the Similarity transform
-  double Sc;
+  double Sc = 0;
   Mat3 Rc;
   Vec3 tc;
   FindRTS(x1, x2, &Sc, &tc, &Rc);
-  Refine_RTS(x1,x2,&Sc,&tc,&Rc);
+  Refine_RTS(x1, x2, &Sc, &tc, &Rc);
 
   std::cout << "\n"
-    << "Scale " << Sc << "\n"
-    << "Rot \n" << Rc << "\n"
-    << "t " << tc.transpose();
+          << "Scale " << Sc << "\n"
+          << "Rot \n" << Rc << "\n"
+          << "t " << tc.transpose();
 
   std::cout << "\nGT\n"
-    << "Scale " << scale << "\n"
-    << "Rot \n" << rot << "\n"
-    << "t " << t.transpose();
+          << "Scale " << scale << "\n"
+          << "Rot \n" << rot << "\n"
+          << "t " << t.transpose();
 }
 
-TEST(SRT_precision, Experiment_ScaleRotTranslation) {
+TEST(SRT_precision, Experiment_ScaleRotTranslation)
+{
 
-  int nbPoints = 10;
-  Mat x1 = Mat::Random(3,nbPoints);
+  const std::size_t nbPoints = 10;
+  Mat x1 = Mat::Random(3, nbPoints);
   Mat x2 = x1;
 
-  double scale = 2;
+  const double scale = 2;
   Mat3 rot = (Eigen::AngleAxis<double>(.2, Vec3::UnitX())
-      * Eigen::AngleAxis<double>(.3, Vec3::UnitY())
-      * Eigen::AngleAxis<double>(.6, Vec3::UnitZ())).toRotationMatrix();
-  Vec3 t(0.5,-0.3,.38);
+          * Eigen::AngleAxis<double>(.3, Vec3::UnitY())
+          * Eigen::AngleAxis<double>(.6, Vec3::UnitZ())).toRotationMatrix();
+  Vec3 t(0.5, -0.3, .38);
 
-  for (int i=0; i < nbPoints; ++i)
+  for(std::size_t i = 0; i < nbPoints; ++i)
   {
     Vec3 pt = x1.col(i);
+    x2.col(i) = (scale * rot * pt + t);
+  }
+
+  // Compute the Similarity transform
+  double Sc = 0;
+  Mat3 Rc;
+  Vec3 tc;
+  FindRTS(x1, x2, &Sc, &tc, &Rc);
+  Refine_RTS(x1, x2, &Sc, &tc, &Rc);
+
+  std::cout << "\n"
+          << "Scale " << Sc << "\n"
+          << "Rot \n" << Rc << "\n"
+          << "t " << tc.transpose();
+
+  std::cout << "\nGT\n"
+          << "Scale " << scale << "\n"
+          << "Rot \n" << rot << "\n"
+          << "t " << t.transpose();
+}
+
+TEST(SRT_precision, ACRANSAC_noNoise)
+{
+  const std::size_t nbPoints = 100;
+  Mat x1 = Mat::Random(3, nbPoints);
+  Mat x2 = x1;
+
+  const double scale = 2;
+  Mat3 rot = (Eigen::AngleAxis<double>(.2, Vec3::UnitX())
+          * Eigen::AngleAxis<double>(.3, Vec3::UnitY())
+          * Eigen::AngleAxis<double>(.6, Vec3::UnitZ())).toRotationMatrix();
+  Vec3 t(0.5, -0.3, .38);
+
+  for(std::size_t i = 0; i < nbPoints; ++i)
+  {
+    const Vec3 &pt = x1.col(i);
     x2.col(i) = (scale * rot * pt + t);
   }
 
@@ -100,20 +144,108 @@ TEST(SRT_precision, Experiment_ScaleRotTranslation) {
   double Sc;
   Mat3 Rc;
   Vec3 tc;
-  FindRTS(x1, x2, &Sc, &tc, &Rc);
-  Refine_RTS(x1,x2,&Sc,&tc,&Rc);
+
+  std::vector<std::size_t> vec_inliers;
+  const bool result = ACRansac_FindRTS(x1, x2, Sc, tc, Rc, vec_inliers, true);
+
+  EXPECT_TRUE(result);
+  EXPECT_TRUE(vec_inliers.size() == nbPoints);
 
   std::cout << "\n"
-    << "Scale " << Sc << "\n"
-    << "Rot \n" << Rc << "\n"
-    << "t " << tc.transpose();
+          << "Scale " << Sc << "\n"
+          << "Rot \n" << Rc << "\n"
+          << "t " << tc.transpose();
 
   std::cout << "\nGT\n"
-    << "Scale " << scale << "\n"
-    << "Rot \n" << rot << "\n"
-    << "t " << t.transpose();
+          << "Scale " << scale << "\n"
+          << "Rot \n" << rot << "\n"
+          << "t " << t.transpose();
+
+  EXPECT_NEAR(scale, Sc, 1e-9);
+  
+  Mat4 RTS;
+  composeRTS(Sc, tc, Rc, RTS);
+
+  for(std::size_t i = 0; i < nbPoints; ++i)
+  {
+    const double error = geometry::RTSSquaredResidualError::Error(RTS, x1.col(i), x2.col(i));
+    EXPECT_NEAR(error, 0.0, 1e-9);
+  }
+}
+
+TEST(SRT_precision, ACRANSAC_noiseByShuffling)
+{
+  // it generates some points x1, it only generates the corresponding 
+  // transformed points x2 for nbPoints-nbShuffles of them while the rest
+  // are again taken randomly in order to generate outliers
+  const std::size_t nbPoints = 100;
+  const std::size_t nbNoisy = 50;
+
+  Mat x1 = Mat::Random(3, nbPoints);
+  Mat x2 = Mat::Random(3, nbPoints);
+
+  const double scale = 2;
+  Mat3 rot = (Eigen::AngleAxis<double>(.2, Vec3::UnitX())
+          * Eigen::AngleAxis<double>(.3, Vec3::UnitY())
+          * Eigen::AngleAxis<double>(.6, Vec3::UnitZ())).toRotationMatrix();
+  Vec3 t(0.5, -0.3, .38);
+
+  for(std::size_t i = 0; i < nbPoints - nbNoisy; ++i)
+  {
+    const Vec3 &pt = x1.col(i);
+    x2.col(i) = (scale * rot * pt + t);
+  }
+
+  // Compute the Similarity transform
+  double Sc;
+  Mat3 Rc;
+  Vec3 tc;
+
+  std::vector<std::size_t> vec_inliers;
+  const bool result = ACRansac_FindRTS(x1, x2, Sc, tc, Rc, vec_inliers, true);
+
+  std::cout << "\n"
+          << "Scale " << Sc << "\n"
+          << "Rot \n" << Rc << "\n"
+          << "t " << tc.transpose();
+
+  std::cout << "\nGT\n"
+          << "Scale " << scale << "\n"
+          << "Rot \n" << rot << "\n"
+          << "t " << t.transpose() << "\n";
+  
+  EXPECT_TRUE(result);
+  // all the points must be inliers (no noise)
+  const std::size_t nbInliers = vec_inliers.size();
+  EXPECT_TRUE(nbInliers == nbPoints - nbNoisy);
+
+  Mat inliers1 = Mat3X(3, nbInliers);
+  Mat inliers2 = Mat3X(3, nbInliers);
+
+  for(std::size_t i = 0; i < nbInliers; ++i)
+  {
+    inliers1.col(i) = x1.col(vec_inliers[i]);
+    inliers2.col(i) = x2.col(vec_inliers[i]);
+  }
+
+  // check scale
+  EXPECT_NEAR(scale, Sc, 1e-9);
+  
+  Mat4 RTS;
+  composeRTS(Sc, tc, Rc, RTS);
+
+  // check the residuals for the inliers
+  for(std::size_t i = 0; i < nbInliers; ++i)
+  {
+    const double error = geometry::RTSSquaredResidualError::Error(RTS, inliers1.col(i), inliers2.col(i));
+    EXPECT_NEAR(error, 0.0, 1e-9);
+  }
 }
 
 /* ************************************************************************* */
-int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+int main()
+{
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
 /* ************************************************************************* */
