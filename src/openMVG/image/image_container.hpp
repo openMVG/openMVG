a1a2494218b11ba395665e779979cb3bc3f41033
pmoulon <pmoulon@gmail.com>
2016-01-28 18:29:43 +0100

    [image] Enhance doxygen documentation #463  - Thanks to @rperrot.

--- src/openMVG/image/image_container.hpp
+++ src/openMVG/image/image_container.hpp
@@ -24,72 +24,229 @@
 //-- Pixel access is done with operator(y,x)
 //  [2/3/2011 pierre MOULON]
 //---------------------------
-namespace openMVG {
-namespace image {
-
-template <typename T>
-class Image : public Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+namespace openMVG
 {
-
-public:
-  typedef T Tpixel;	//-- Pixel data type
-  typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Base;
-
-  //------------------------------
-  //-- Image construction method
-  inline Image() {Base::resize(0, 0);}
-
-  inline Image(int width, int height, bool fInit=true, const T val = T())
+  namespace image
   {
-    Base::resize(height, width);
-    if (fInit) Base::fill(val);
-  };
 
-  inline Image(const Base& I)  { (*this) = I; }
-  inline Image& operator=(const Base& I) { Base::operator=(I);  return *this; }
-  virtual inline ~Image() {}
-  //-- Image construction method
-  //------------------------------
+    template <typename T>
+    class Image : public Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+    {
 
-  //-- Resize overhead
-  inline void resize(int width, int height, bool fInit=true, const T val = T(0))
-  {
-    Base::resize(height, width);
-    if (fInit) Base::fill(val);
-  }
+    public:
+      typedef T Tpixel; //-- Pixel data type
+      typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Base;
 
-  //------------------------------
-  //-- accessors/getters methods
-  /// Retrieve the width of the image
-  inline int Width()  const { return static_cast<int>(Base::cols()); }
-  /// Retrieve the height of the image
-  inline int Height() const { return static_cast<int>(Base::rows()); }
-  /// Return the depth in byte of the pixel (unsigned char will return 1)
-  inline int Depth() const  { return sizeof(Tpixel); }
 
-  /// random pixel access
-  inline const T& operator()(int y, int x) const { return Base::operator()(y,x); }
-  /// random pixel access
-  inline T& operator()(int y, int x) { return Base::operator()(y,x); }
-  // sub-pixel access
-  //T operator()(double x, double y, Tinterpolation eInterpMode = Image::INTER_BILINEAR) const;
+      /**
+       * @brief Default constructor
+       * @note This create an empty image
+       */
+      inline Image()
+      {
+        Base::resize( 0, 0 );
+      }
 
-  inline const Base& GetMat() const { return (*this); }
+      /**
+      * @brief Full constructor
+      * @param width Width of the image (ie number of column)
+      * @param height Height of the image (ie number of row)
+      * @param fInit Tell if the image should be initialized
+      * @param val If fInit is true, set all pixel to the specified value
+      */
+      inline Image( int width, int height, bool fInit = true, const T val = T() )
+      {
+        Base::resize( height, width );
+        if ( fInit )
+        {
+          Base::fill( val );
+        }
+      };
 
-  //-- accessors/getters methods
-  //------------------------------
+      /**
+      * @brief Copy constructor
+      * @param I Source image
+      */
+      inline Image( const Base& I )
+        : Base( I )
+      {
 
-  /// Tell if a point is inside the image.
-  inline bool Contains(int y, int x) const  {
-    return 0 <= x && x < Base::cols()
-      && 0 <= y && y < Base::rows();
-  }
+      }
 
-protected :
-  //-- Image data are stored by inheritance of a matrix
-};
+      /**
+      * @brief Move constructor
+      * @param src Source image
+      */
+      inline Image( Base && src )
+        : Base( std::move( src ) )
+      {
 
-} // namespace image
+      }
+
+      /**
+      * @brief Assignment operator
+      * @param I Source image
+      * @return Image after assignment
+      */
+      inline Image& operator=( const Base& I )
+      {
+        Base::operator=( I );
+        return *this;
+      }
+
+      /**
+      * @brief destructor
+      */
+      virtual inline ~Image() {};
+      //-- Image construction method
+      //------------------------------
+
+
+      /**
+      * @brief Change geometry of image
+      * @param width New width of image
+      * @param height New height of image
+      * @param fInit Indicate if new image should be initialized
+      * @param val if fInit is true all pixel in the new image are set to this value
+      */
+      inline void resize( int width, int height, bool fInit = true, const T val = T( 0 ) )
+      {
+        Base::resize( height, width );
+        if ( fInit )
+        {
+          Base::fill( val );
+        }
+      }
+
+      //------------------------------
+      //-- accessors/getters methods
+      /**
+       * @brief Retrieve the width of the image
+       * @return Width of image
+       */
+      inline int Width()  const
+      {
+        return static_cast<int>( Base::cols() );
+      }
+
+      /**
+       * @brief Retrieve the height of the image
+       * @return Height of the image
+       */
+      inline int Height() const
+      {
+        return static_cast<int>( Base::rows() );
+      }
+
+      /**
+      * @brief Return the depth in byte of the pixel
+      * @return depth of the pixel (in byte)
+      * @note (T=unsigned char will return 1)
+      */
+      inline int Depth() const
+      {
+        return sizeof( Tpixel );
+      }
+
+      /**
+      * @brief constant random pixel access
+      * @param y Index of the row
+      * @param x Index of the column
+      * @return Constant pixel reference at position (y,x)
+      */
+      inline const T& operator()( int y, int x ) const
+      {
+        return Base::operator()( y, x );
+      }
+
+      /**
+       * @brief random pixel access
+       * @param y Index of the row
+       * @param x Index of the column
+       * @return Pixel reference at position (y,x)
+       */
+      inline T& operator()( int y, int x )
+      {
+        return Base::operator()( y, x );
+      }
+
+      /**
+      * @brief Get low level access to the internal pixel data
+      * @return const reference to internal matrix data
+      */
+      inline const Base& GetMat() const
+      {
+        return ( *this );
+      }
+
+      //-- accessors/getters methods
+      //------------------------------
+
+      /**
+      * @brief Tell if a point is inside the image.
+      * @param y Index of the row
+      * @param x Index of the column
+      * @retval true If pixel (y,x) is inside the image
+      * @retval false If pixel (y,x) is outside the image
+      */
+      inline bool Contains( int y, int x ) const
+      {
+        return 0 <= x && x < Base::cols()
+               && 0 <= y && y < Base::rows();
+      }
+
+      /**
+      * @brief Pixelwise addition of two images
+      * @param imgA First image
+      * @param imgB Second image
+      * @return pixelwise imgA + imgB
+      * @note Images must have the same size
+      */
+      template< typename T1>
+      friend Image<T1> operator+( const Image<T1> & imgA , const Image<T1> & imgB ) ;
+
+      /**
+      * @brief Pixelwise subtraction of two images
+      * @param imgA First image
+      * @param imgB Second image
+      * @return pixelwise imgA - imgB
+      * @note Images must have the same size
+      */
+      template< typename T1>
+      friend Image<T1> operator-( const Image<T1> & imgA , const Image<T1> & imgB ) ;
+
+
+    protected :
+      //-- Image data are stored by inheritance of a matrix
+    };
+
+    /**
+    * @brief Pixelwise addition of two images
+    * @param imgA First image
+    * @param imgB Second image
+    * @return pixelwise imgA + imgB
+    * @note Images must have the same size
+    */
+    template< typename T1 >
+    Image<T1> operator+( const Image<T1> & imgA , const Image<T1> & imgB )
+    {
+      return Image<T1>( imgA.Image<T1>::operator+( imgB ) ) ;
+    }
+
+    /**
+    * @brief Pixelwise subtraction of two images
+    * @param imgA First image
+    * @param imgB Second image
+    * @return pixelwise imgA - imgB
+    * @note Images must have the same size
+    */
+    template< typename T1>
+    Image<T1> operator-( const Image<T1> & imgA , const Image<T1> & imgB )
+    {
+      return Image<T1>( imgA.Image<T1>::operator-( imgB ) ) ;
+    }
+
+  } // namespace image
 } // namespace openMVG
 
 #endif // OPENMVG_IMAGE_IMAGE_HPP
