72af7d17e2af557e5a3e8b1de08fb410e4109c3c
Romuald PERROT <perrot.romuald@gmail.com>
2016-02-23 16:11:40 +0100

    - Add documentation for basic openMVG types

--- src/openMVG/types.hpp
+++ src/openMVG/types.hpp
@@ -16,27 +16,51 @@
 #include <vector>
 
 #if defined OPENMVG_STD_UNORDERED_MAP
-#include <unordered_map>
+  #include <unordered_map>
 #endif
 
-namespace openMVG{
+/**
+* @brief Main namespace of openMVG API
+*/
+namespace openMVG
+{
 
+/// Portable type used to store an index
 typedef uint32_t IndexT;
+
+/// Portable value used to save an undefined index value
 static const IndexT UndefinedIndexT = std::numeric_limits<IndexT>::max();
 
-typedef std::pair<IndexT,IndexT> Pair;
+/// Standard Pair of int
+typedef std::pair<IndexT, IndexT> Pair;
+
+/// A set of int-pair
 typedef std::set<Pair> Pair_Set;
+
+/// Vector of Pairs
 typedef std::vector<Pair> Pair_Vec;
 
 #define OPENMVG_NO_UNORDERED_MAP 1
 
 #if defined OPENMVG_NO_UNORDERED_MAP
+
+/**
+* @brief Standard Hash_Map class
+* @tparam K type of the keys
+* @tparam V type of the values
+*/
 template<typename K, typename V>
 struct Hash_Map : std::map<K, V, std::less<K>,
- Eigen::aligned_allocator<std::pair<K,V> > > {};
+  Eigen::aligned_allocator<std::pair<K, V> > > {};
 #endif
 
 #if defined OPENMVG_STD_UNORDERED_MAP
+
+/**
+* @brief Standard Hash_Map class
+* @tparam K type of the keys
+* @tparam V type of the values
+*/
 template<typename Key, typename Value>
 struct Hash_Map : std::unordered_map<Key, Value> {};
 #endif
