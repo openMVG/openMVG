7d34d0c6a2c024b85ac015699984b1e3bbdd68ce
pmoulon <pmoulon@gmail.com>
2016-03-18 11:08:57 +0100

    [exif] Enhance EXIF parsing & camera database checking - Mapir cameras. #505 - Simplify the sensor width database - Add PEAU PRODUCTIONS MAPIR camera - Simplify the Datasheet comparison Since camera model already contains the branch; only the camera model is now used.

--- src/software/SfM/main_SfMInit_ImageListing.cpp
+++ src/software/SfM/main_SfMInit_ImageListing.cpp
@@ -205,12 +205,11 @@ int main(int argc, char **argv)
     ppx = width / 2.0;
     ppy = height / 2.0;
 
-    std::unique_ptr<Exif_IO> exifReader(new Exif_IO_EasyExif());
+    std::unique_ptr<Exif_IO> exifReader(new Exif_IO_EasyExif);
     exifReader->open( sImageFilename );
 
     const bool bHaveValidExifMetadata =
       exifReader->doesHaveExifInfo()
-      && !exifReader->getBrand().empty()
       && !exifReader->getModel().empty();
 
     // Consider the case where the focal is provided manually
@@ -227,7 +226,6 @@ int main(int argc, char **argv)
     }
     else // If image contains meta data
     {
-      const std::string sCamName = exifReader->getBrand();
       const std::string sCamModel = exifReader->getModel();
 
       // Handle case where focal length is equal to 0
@@ -241,17 +239,17 @@ int main(int argc, char **argv)
       // Create the image entry in the list file
       {
         Datasheet datasheet;
-        if ( getInfo( sCamName, sCamModel, vec_database, datasheet ))
+        if ( getInfo( sCamModel, vec_database, datasheet ))
         {
           // The camera model was found in the database so we can compute it's approximated focal length
-          const double ccdw = datasheet._sensorSize;
+          const double ccdw = datasheet.sensorSize_;
           focal = std::max ( width, height ) * exifReader->getFocal() / ccdw;
         }
         else
         {
           error_report_stream
-            << stlplus::basename_part(sImageFilename) << ": Camera \""
-            << sCamName << "\" model \"" << sCamModel << "\" doesn't exist in the database" << "\n"
+            << stlplus::basename_part(sImageFilename)
+            << "\" model \"" << sCamModel << "\" doesn't exist in the database" << "\n"
             << "Please consider add your camera model and sensor width in the database." << "\n";
         }
       }
