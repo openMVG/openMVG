76b4f98cc506978f3a739f2aa72129e3b3dd87fc
Simone Gasparini <simone.gasparini@gmail.com>
2016-07-29 10:20:34 +0200

    [sfm] SfM_Localizer removed unused define

--- src/openMVG/sfm/pipelines/localization/SfM_Localizer.cpp
+++ src/openMVG/sfm/pipelines/localization/SfM_Localizer.cpp
@@ -5,8 +5,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#define USE_ACRANSAC
-
 #include "openMVG/sfm/pipelines/localization/SfM_Localizer.hpp"
 #include "openMVG/sfm/sfm_data_BA_ceres.hpp"
 
