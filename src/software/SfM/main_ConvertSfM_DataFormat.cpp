b7b422110912581f898642d7c9e66d394bc34a9c
Simone Gasparini <simone.gasparini@gmail.com>
2016-08-02 18:08:33 +0200

    [software] added option to (re-)compute UID while converting
    this is useful to convert legacy reconstructions

--- src/software/SfM/main_ConvertSfM_DataFormat.cpp
+++ src/software/SfM/main_ConvertSfM_DataFormat.cpp
@@ -5,6 +5,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include "openMVG/sfm/sfm.hpp"
+#include "openMVG/exif/exif_IO_EasyExif.hpp"
 
 #include "third_party/cmdLine/cmdLine.h"
 #include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
@@ -14,6 +15,108 @@
 
 using namespace openMVG;
 using namespace openMVG::sfm;
+
+void updateStructureWithNewUID(Landmarks &landmarks, const std::map<std::size_t, std::size_t> &oldIdToNew)
+{
+  // update the id in the visibility of each 3D point
+  for(auto &iter : landmarks)
+  {
+    Landmark& currentLandmark = iter.second;
+    
+    // the new observations where to copy the existing ones
+    // (needed as the key of the map is the idview)
+    Observations newObservations;
+    
+    for(const auto &iterObs : currentLandmark.obs)
+    {
+      const auto idview = iterObs.first;
+      const Observation &obs = iterObs.second;
+
+      newObservations.emplace(oldIdToNew.at(idview), obs);
+    }
+    
+    assert(currentLandmark.obs.size() == newObservations.size());
+    currentLandmark.obs.swap(newObservations);
+  }  
+}
+
+void regenerateUID(sfm::SfM_Data &sfmdata, bool sanityCheck = false)
+{
+  // if the views are empty, nothing to be done. 
+  if(sfmdata.GetViews().empty())
+    return;
+  
+  // this is used to map the old viewID to the UID for each view
+  std::map<std::size_t, std::size_t> oldIdToNew;
+  
+  Views newViews;
+
+  for(auto const &iter : sfmdata.views)
+  {
+    const View& currentView = *iter.second.get();
+    const auto &imageName = currentView.s_Img_path;
+    
+    exif::Exif_IO_EasyExif exifReader(imageName);
+
+    // compute the view UID
+    const std::size_t uid = exif::computeUID(exifReader, imageName);
+
+    // update the mapping
+    assert(oldIdToNew.count(currentView.id_view) == 0);
+    oldIdToNew.emplace(currentView.id_view, uid);
+    
+    // add the view to the new map using the uid as key and change the id
+    assert(newViews.count(uid) == 0);
+    newViews.emplace(uid, iter.second);
+    newViews[uid]->id_view = uid;
+  }
+  
+  assert(newViews.size() == sfmdata.GetViews().size());
+  sfmdata.views.swap(newViews);
+  
+  
+  // update the id in the visibility of each 3D point
+  updateStructureWithNewUID(sfmdata.structure, oldIdToNew);
+  
+   // update the id in the visibility of each 3D point
+  updateStructureWithNewUID(sfmdata.control_points, oldIdToNew);
+  
+  if(!sanityCheck)
+    return;
+  
+  // sanity check
+  for(auto &iter : sfmdata.structure)
+  {
+    Landmark& currentLandmark = iter.second;
+    for(const auto &iterObs : currentLandmark.obs)
+    {
+      const auto idview = iterObs.first;
+      const Observation &obs = iterObs.second;
+
+      // there must be a view with that id (in the map) and the view must have 
+      // the same id (the member)
+      assert(sfmdata.views.count(idview) == 1);
+      assert(sfmdata.views.at(idview)->id_view == idview);
+    }
+  }  
+  
+  // sanity check
+  for(auto &iter : sfmdata.control_points)
+  {
+    Landmark& currentLandmark = iter.second;
+    for(const auto &iterObs : currentLandmark.obs)
+    {
+      const auto idview = iterObs.first;
+      const Observation &obs = iterObs.second;
+
+      // there must be a view with that id (in the map) and the view must have 
+      // the same id (the member)
+      assert(sfmdata.views.count(idview) == 1);
+      assert(sfmdata.views.at(idview)->id_view == idview);
+    }
+  }
+  
+}
 
 // Convert from a SfM_Data format to another
 int main(int argc, char **argv)
@@ -31,6 +134,7 @@ int main(int argc, char **argv)
   cmd.add(make_switch('S', "STRUCTURE"));
   cmd.add(make_switch('O', "OBSERVATIONS"));
   cmd.add(make_switch('C', "CONTROL_POINTS"));
+  cmd.add(make_switch('u', "uid"));
   cmd.add(make_option('o', sSfM_Data_Filename_Out, "output_file"));
 
   try {
@@ -53,6 +157,7 @@ int main(int argc, char **argv)
         << "[-S|--STRUCTURE] export structure\n"
         << "[-O|--OBSERVATIONS] export 2D observations associated with 3D structure\n"
         << "[-C|--CONTROL_POINTS] export control points\n"
+        << "[-u|--uid] (re-)compute the unique ID (UID) for the views\n"
         << std::endl;
 
       std::cerr << s << std::endl;
@@ -75,6 +180,8 @@ int main(int argc, char **argv)
   | (cmd.used('S') ? STRUCTURE  : 0);
 
   flags = (flags) ? flags : ALL;
+  
+  const bool recomputeUID = cmd.used('u');
 
   // Load input SfM_Data scene
   SfM_Data sfm_data;
@@ -84,9 +191,15 @@ int main(int argc, char **argv)
       << "The input SfM_Data file \"" << sSfM_Data_Filename_In << "\" cannot be read." << std::endl;
     return EXIT_FAILURE;
   }
+  
+  if(recomputeUID)
+  {
+    std::cout << "Recomputing the UID of the views..." << std::endl;
+    regenerateUID(sfm_data);
+  }
 
   // Export the SfM_Data scene in the expected format
-  if (!Save(sfm_data, sSfM_Data_Filename_Out.c_str(), ESfM_Data(flags)))
+  if (!Save(sfm_data, sSfM_Data_Filename_Out, ESfM_Data(flags)))
   {
     std::cerr << std::endl
       << "An error occured while trying to save \"" << sSfM_Data_Filename_Out << "\"." << std::endl;
