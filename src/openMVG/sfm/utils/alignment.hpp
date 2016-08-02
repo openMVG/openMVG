7fc6ff67c80f22904141fddab676776fd6fbce79
Simone Gasparini <simone.gasparini@gmail.com>
2016-08-02 18:03:19 +0200

    [sfm] plugged commonViewsWithPose and moved to cpp
    fix poparteu/openMVG#201

--- src/openMVG/sfm/utils/alignment.hpp
+++ src/openMVG/sfm/utils/alignment.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "openMVG/sfm/sfm_data.hpp"
-#include "openMVG/geometry/rigid_transformation3D_srt.hpp"
+#include "openMVG/geometry/pose3.hpp"
 
 namespace openMVG {
 namespace sfm {
@@ -48,47 +48,17 @@ inline void getCommonViewsWithPoses(const SfM_Data & sfmDataA,
  * @param[out] out_t: output translation vector
  * @return true if it finds a similarity transformation
  */
-inline bool computeSimilarity(
-  const SfM_Data & sfmDataA,
-  const SfM_Data & sfmDataB,
-  double * out_S, Mat3 * out_R, Vec3 * out_t)
-{
-  std::vector<IndexT> commonViewIds;
-  getCommonViews(sfmDataA, sfmDataB, commonViewIds);
-  if(commonViewIds.size() < 2)
-  {
-    std::cerr << "Cannot compute similarities. Need at least 2 common views." << std::endl;
-    return false;
-  }
+bool computeSimilarity(const SfM_Data & sfmDataA,
+                       const SfM_Data & sfmDataB,
+                       double * out_S,
+                       Mat3 * out_R,
+                       Vec3 * out_t);
 
-  // Move input point in appropriate container
-  Mat xA(3, commonViewIds.size());
-  Mat xB(3, commonViewIds.size());
-  for (size_t i = 0; i  < commonViewIds.size(); ++i)
-  {
-    IndexT viewId = commonViewIds[i];
-    xA.col(i) = sfmDataA.GetPoses().at(sfmDataA.GetViews().at(viewId)->id_pose).center();
-    xB.col(i) = sfmDataB.GetPoses().at(sfmDataB.GetViews().at(viewId)->id_pose).center();
-  }
-
-  // Compute rigid transformation p'i = S R pi + t
-  double S;
-  Vec3 t;
-  Mat3 R;
-  std::vector<std::size_t> inliers;
-  if(!openMVG::geometry::ACRansac_FindRTS(xA, xB, S, t, R, inliers, true))
-    return false;
-
-  std::cout << "There are " << commonViewIds.size() << " common cameras and " << inliers.size() << " were used to compute the similarity transform." << std::endl;
-
-  *out_S = S;
-  *out_R = R;
-  *out_t = t;
-  return true;
-}
 
 inline void applyTransform(SfM_Data & sfmData,
-    const double S, const Mat3& R, const Vec3& t)
+                           const double S,
+                           const Mat3& R,
+                           const Vec3& t)
 {
   for(auto& view: sfmData.views)
   {
