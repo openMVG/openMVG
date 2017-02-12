b52a5afa6f8c504fde6225b49ab264d0db767d87
pmoulon <pmoulon@gmail.com>
2017-02-12 07:08:42 +0100

    cosmetic

--- src/openMVG/matching/matching_filters_test.cpp
+++ src/openMVG/matching/matching_filters_test.cpp
@@ -17,10 +17,10 @@ using namespace std;
 /// Sorted vector intersection (increasing order)
 TEST( matching, setIntersection)
 {
-  int tab0[] = {0, 1, 2, 3, 4, 5, 6, 7};
-  int tab1[] = {0, 1, 8, 3, 4, 9, 6, 7};
-  set<int> vec_0(tab0, tab0+8);
-  set<int> vec_1(tab1, tab1+8);
+  const int tab0[] = {0, 1, 2, 3, 4, 5, 6, 7};
+  const int tab1[] = {0, 1, 8, 3, 4, 9, 6, 7};
+  const set<int> vec_0(tab0, tab0+8);
+  const set<int> vec_1(tab1, tab1+8);
   /// Array must be sorted
 
   vector<int> vec_intersect;
