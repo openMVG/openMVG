openMVG/openMVG

commit e833adcf792d702330114061c142000d6d0a0356

Moved all uses of cereal to headers with the suffix '_io.hpp'. #911

src/openMVG/sfm/sfm_data_io_cereal.cpp
@@ -6,19 +6,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// The <cereal/archives> headers are special and must be included first.
+#include <cereal/archives/binary.hpp>
+#include <cereal/archives/json.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/archives/xml.hpp>
+
 #include "openMVG/sfm/sfm_data_io_cereal.hpp"
 
-#include "openMVG/cameras/cameras.hpp"
+#include "openMVG/cameras/cameras_io.hpp"
+#include "openMVG/geometry/pose3_io.hpp"
 #include "openMVG/sfm/sfm_data.hpp"
+#include "openMVG/sfm/sfm_landmark_io.hpp"
+#include "openMVG/sfm/sfm_view_io.hpp"
 #include "openMVG/types.hpp"
 
 #include <fstream>
 #include <string>
 
-#include <cereal/archives/binary.hpp>
-#include <cereal/archives/json.hpp>
-#include <cereal/archives/portable_binary.hpp>
-#include <cereal/archives/xml.hpp>
 #include <cereal/types/map.hpp>
 #include <cereal/types/string.hpp>
 #include <cereal/types/unordered_map.hpp>

src/openMVG/sfm/sfm_view_priors_io.hpp
@@ -1,0 +1,94 @@
+// This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
+
+// Copyright (c) 2016 Pierre Moulon.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OPENMVG_SFM_SFM_VIEW_PRIORS_HPP
+#define OPENMVG_SFM_SFM_VIEW_PRIORS_HPP
+
+#include <cereal/types/vector.hpp>
+#include <cereal/types/polymorphic.hpp>
+
+#include "openMVG/sfm/sfm_view_priors_io.hpp"
+
+template <class Archive>
+void openMVG::sfm::ViewPriors::save( Archive & ar ) const
+{
+  View::save(ar);
+
+  // Pose center prior
+  if (b_use_pose_center_)
+  {
+    ar( cereal::make_nvp( "use_pose_center_prior", b_use_pose_center_ ) );
+    const std::vector<double> vec_weights = { center_weight_( 0 ), center_weight_( 1 ), center_weight_( 2 ) };
+    ar( cereal::make_nvp( "center_weight", vec_weights ) );
+    const std::vector<double> vec = { pose_center_( 0 ), pose_center_( 1 ), pose_center_( 2 ) };
+    ar( cereal::make_nvp( "center", vec ) );
+  }
+
+  // Pose rotation prior
+  /*
+  if (b_use_pose_rotation_)
+  {
+    ar( cereal::make_nvp( "use_pose_rotation_prior", b_use_pose_rotation_ ) );
+    ar( cereal::make_nvp( "rotation_weight", rotation_weight_ ) );
+    const std::vector<std::vector<double>> mat =
+    {
+      { pose_rotation_( 0, 0 ), pose_rotation_( 0, 1 ), pose_rotation_( 0, 2 ) },
+      { pose_rotation_( 1, 0 ), pose_rotation_( 1, 1 ), pose_rotation_( 1, 2 ) },
+      { pose_rotation_( 2, 0 ), pose_rotation_( 2, 1 ), pose_rotation_( 2, 2 ) }
+    };
+    ar( cereal::make_nvp( "rotation", mat ) );
+  }
+  */
+}
+
+template <class Archive>
+void openMVG::sfm::ViewPriors::load( Archive & ar )
+{
+  View::load(ar);
+
+  // Pose center prior
+  try
+  {
+    ar( cereal::make_nvp( "use_pose_center_prior", b_use_pose_center_ ) );
+    std::vector<double> vec( 3 );
+    ar( cereal::make_nvp( "center_weight", vec ) );
+    center_weight_ = Eigen::Map<const Vec3>( &vec[0] );
+    ar( cereal::make_nvp( "center", vec ) );
+    pose_center_ = Eigen::Map<const Vec3>( &vec[0] );
+  }
+  catch( cereal::Exception e )
+  {
+    // if it fails just use a default settings
+    b_use_pose_center_ = false;
+  }
+
+  // Pose rotation prior
+  /*
+  try
+  {
+    ar( cereal::make_nvp( "use_pose_rotation_prior", b_use_pose_rotation_ ) );
+    ar( cereal::make_nvp( "rotation_weight", rotation_weight_ ) );
+    std::vector<std::vector<double>> mat( 3, std::vector<double>( 3 ) );
+    ar( cereal::make_nvp( "rotation", mat ) );
+    // copy back to the rotation
+    pose_rotation_.row( 0 ) = Eigen::Map<const Vec3>( &( mat[0][0] ) );
+    pose_rotation_.row( 1 ) = Eigen::Map<const Vec3>( &( mat[1][0] ) );
+    pose_rotation_.row( 2 ) = Eigen::Map<const Vec3>( &( mat[2][0] ) );
+  }
+  catch( const cereal::Exception & e )
+  {
+    // if it fails just use a default settings
+    b_use_pose_rotation_ = false;
+  }
+  */
+}
+
+CEREAL_REGISTER_TYPE_WITH_NAME( openMVG::sfm::ViewPriors, "view_priors" );
+CEREAL_REGISTER_POLYMORPHIC_RELATION(openMVG::sfm::View, openMVG::sfm::ViewPriors);
+
+#endif // OPENMVG_SFM_SFM_VIEW_PRIORS_HPP

src/nonFree/sift/SIFT_describer.hpp
@@ -66,16 +66,7 @@ public:
       _root_sift(root_sift) {}
 
     template<class Archive>
-    void serialize( Archive & ar )
-    {
-      ar(
-        cereal::make_nvp("first_octave", _first_octave),
-        cereal::make_nvp("num_octaves",_num_octaves),
-        cereal::make_nvp("num_scales",_num_scales),
-        cereal::make_nvp("edge_threshold",_edge_threshold),
-        cereal::make_nvp("peak_threshold",_peak_threshold),
-        cereal::make_nvp("root_sift",_root_sift));
-    }
+    inline void serialize( Archive & ar );
 
     // Parameters
     int _first_octave;      // Use original image, or perform an upscale if == -1
@@ -221,12 +212,7 @@ public:
   }
 
   template<class Archive>
-  void serialize( Archive & ar )
-  {
-    ar(
-     cereal::make_nvp("params", _params),
-     cereal::make_nvp("bOrientation", _bOrientation));
-  }
+  inline void serialize( Archive & ar );
 
 private:
   Params _params;
@@ -235,10 +221,5 @@ private:
 
 } // namespace features
 } // namespace openMVG
-
-#include <cereal/types/polymorphic.hpp>
-#include <cereal/archives/json.hpp>
-CEREAL_REGISTER_TYPE_WITH_NAME(openMVG::features::SIFT_Image_describer, "SIFT_Image_describer");
-CEREAL_REGISTER_POLYMORPHIC_RELATION(openMVG::features::Image_describer, openMVG::features::SIFT_Image_describer)
 
 #endif // OPENMVG_PATENTED_SIFT_SIFT_DESCRIBER_HPP

src/nonFree/sift/SIFT_describer_io.hpp
@@ -1,0 +1,39 @@
+// This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
+
+// Copyright (c) 2015 Pierre MOULON.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OPENMVG_PATENTED_SIFT_SIFT_DESCRIBER_IO_HPP
+#define OPENMVG_PATENTED_SIFT_SIFT_DESCRIBER_IO_HPP
+
+#include "nonFree/sift/SIFT_describer.hpp"
+
+#include <cereal/types/polymorphic.hpp>
+
+template<class Archive>
+void openMVG::features::SIFT_Image_describer::Params::serialize( Archive & ar )
+{
+  ar(
+    cereal::make_nvp("first_octave", _first_octave),
+    cereal::make_nvp("num_octaves",_num_octaves),
+    cereal::make_nvp("num_scales",_num_scales),
+    cereal::make_nvp("edge_threshold",_edge_threshold),
+    cereal::make_nvp("peak_threshold",_peak_threshold),
+    cereal::make_nvp("root_sift",_root_sift));
+}
+
+template<class Archive>
+void openMVG::features::SIFT_Image_describer::serialize( Archive & ar )
+{
+  ar(
+   cereal::make_nvp("params", _params),
+   cereal::make_nvp("bOrientation", _bOrientation));
+}
+
+CEREAL_REGISTER_TYPE_WITH_NAME(openMVG::features::SIFT_Image_describer, "SIFT_Image_describer");
+CEREAL_REGISTER_POLYMORPHIC_RELATION(openMVG::features::Image_describer, openMVG::features::SIFT_Image_describer)
+
+#endif // OPENMVG_PATENTED_SIFT_SIFT_DESCRIBER_IO_HPP

src/openMVG/cameras/Camera_IO_test.cpp
@@ -6,11 +6,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "openMVG/cameras/Camera_IO.hpp"
-
-#include "openMVG/cameras/cameras.hpp"
+// The <cereal/archives> headers are special and must be included first.
 #include <cereal/archives/json.hpp>
-#include <cereal/cereal.hpp>
+
+#include "openMVG/cameras/Camera_IO.hpp"
+#include "openMVG/cameras/cameras_io.hpp"
 
 using namespace openMVG;
 using namespace openMVG::cameras;

src/openMVG/cameras/Camera_Intrinsics.hpp
@@ -16,8 +16,6 @@
 #include "openMVG/numeric/numeric.h"
 #include "openMVG/stl/hash.hpp"
 
-#include <cereal/types/polymorphic.hpp>
-
 namespace openMVG
 {
 namespace cameras
@@ -217,28 +215,21 @@ struct IntrinsicBase : public Clonable<IntrinsicBase>
   * @return Concatenation of intrinsic matrix and extrinsic matrix
   */
   virtual Mat34 get_projective_equivalent( const geometry::Pose3 & pose ) const = 0;
-
+  
   /**
   * @brief Serialization out
   * @param ar Archive
   */
   template <class Archive>
-  void save( Archive & ar ) const
-  {
-    ar( cereal::make_nvp( "width", w_ ) );
-    ar( cereal::make_nvp( "height", h_ ) );
-  }
+  void save( Archive & ar ) const;
+
 
   /**
   * @brief  Serialization in
   * @param ar Archive
   */
   template <class Archive>
-  void load( Archive & ar )
-  {
-    ar( cereal::make_nvp( "width", w_ ) );
-    ar( cereal::make_nvp( "height", h_ ) );
-  }
+  void load( Archive & ar );
 
   /**
   * @brief Generate a unique Hash from the camera parameters (used for grouping)

src/openMVG/cameras/Camera_Intrinsics_io.hpp
@@ -1,0 +1,28 @@
+// This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
+
+// Copyright (c) 2015 Pierre MOULON.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OPENMVG_CAMERAS_CAMERA_INTRINSICS_IO_HPP
+#define OPENMVG_CAMERAS_CAMERA_INTRINSICS_IO_HPP
+
+#include <cereal/types/polymorphic.hpp>
+
+template <class Archive>
+void openMVG::cameras::IntrinsicBase::save( Archive & ar ) const
+{
+  ar( cereal::make_nvp( "width", w_ ) );
+  ar( cereal::make_nvp( "height", h_ ) );
+}
+
+template <class Archive>
+void openMVG::cameras::IntrinsicBase::load( Archive & ar )
+{
+  ar( cereal::make_nvp( "width", w_ ) );
+  ar( cereal::make_nvp( "height", h_ ) );
+}
+
+#endif // #ifndef OPENMVG_CAMERAS_CAMERA_INTRINSICS_IO_HPP

src/openMVG/cameras/Camera_Pinhole.hpp
@@ -289,13 +289,7 @@ class Pinhole_Intrinsic : public IntrinsicBase
     * @param ar Archive
     */
     template <class Archive>
-    void save( Archive & ar ) const
-    {
-      IntrinsicBase::save(ar);
-      ar( cereal::make_nvp( "focal_length", K_( 0, 0 ) ) );
-      const std::vector<double> pp = {K_( 0, 2 ), K_( 1, 2 )};
-      ar( cereal::make_nvp( "principal_point", pp ) );
-    }
+    inline void save( Archive & ar ) const;
 
 
     /**
@@ -303,15 +297,7 @@ class Pinhole_Intrinsic : public IntrinsicBase
     * @param ar Archive
     */
     template <class Archive>
-    void load( Archive & ar )
-    {
-      IntrinsicBase::load(ar);
-      double focal_length;
-      ar( cereal::make_nvp( "focal_length", focal_length ) );
-      std::vector<double> pp( 2 );
-      ar( cereal::make_nvp( "principal_point", pp ) );
-      *this = Pinhole_Intrinsic( w_, h_, focal_length, pp[0], pp[1] );
-    }
+    inline void load( Archive & ar );
 
     /**
     * @brief Clone the object
@@ -325,9 +311,5 @@ class Pinhole_Intrinsic : public IntrinsicBase
 
 } // namespace cameras
 } // namespace openMVG
-
-#include <cereal/types/polymorphic.hpp>
-CEREAL_REGISTER_TYPE_WITH_NAME(openMVG::cameras::Pinhole_Intrinsic, "pinhole");
-CEREAL_REGISTER_POLYMORPHIC_RELATION(openMVG::cameras::IntrinsicBase, openMVG::cameras::Pinhole_Intrinsic);
 
 #endif // #ifndef OPENMVG_CAMERAS_CAMERA_PINHOLE_HPP

commit 9e7775f5fa407c9ee66611cb8d54bfc6cfee4941

[BugFix/Geodesy] view priors serialization was broken since e833adc

src/openMVG/sfm/sfm_data_io_cereal.cpp
@@ -19,6 +19,7 @@
 #include "openMVG/sfm/sfm_data.hpp"
 #include "openMVG/sfm/sfm_landmark_io.hpp"
 #include "openMVG/sfm/sfm_view_io.hpp"
+#include "openMVG/sfm/sfm_view_priors_io.hpp"
 #include "openMVG/types.hpp"
 
 #include <fstream>

src/openMVG/sfm/sfm_view_priors_io.hpp
@@ -6,13 +6,13 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#ifndef OPENMVG_SFM_SFM_VIEW_PRIORS_HPP
-#define OPENMVG_SFM_SFM_VIEW_PRIORS_HPP
+#ifndef OPENMVG_SFM_SFM_VIEW_PRIORS_IO_HPP
+#define OPENMVG_SFM_SFM_VIEW_PRIORS_IO_HPP
 
-#include <cereal/types/vector.hpp>
+#include "openMVG/sfm/sfm_view_priors.hpp"
+
 #include <cereal/types/polymorphic.hpp>
-
-#include "openMVG/sfm/sfm_view_priors_io.hpp"
+#include <cereal/types/vector.hpp>
 
 template <class Archive>
 void openMVG::sfm::ViewPriors::save( Archive & ar ) const
@@ -91,4 +91,4 @@ void openMVG::sfm::ViewPriors::load( Archive & ar )
 CEREAL_REGISTER_TYPE_WITH_NAME( openMVG::sfm::ViewPriors, "view_priors" );
 CEREAL_REGISTER_POLYMORPHIC_RELATION(openMVG::sfm::View, openMVG::sfm::ViewPriors);
 
-#endif // OPENMVG_SFM_SFM_VIEW_PRIORS_HPP
+#endif // OPENMVG_SFM_SFM_VIEW_PRIORS_IO_HPP