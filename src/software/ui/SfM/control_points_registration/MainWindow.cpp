9adf2897a8a74d55c78ee8ba1c0a09fe70ad0d95
Frits Talbot <frits@metapathy.com>
2017-09-17 15:52:17 +0200

    Rename mainLayout files -> MainWindow to reflect class name

--- src/software/ui/SfM/control_points_registration/MainWindow.cpp
+++ src/software/ui/SfM/control_points_registration/MainWindow.cpp
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "mainLayout.hpp"
+#include "MainWindow.hpp"
 #include <QtGui>
 #include <QMessageBox>
 #include <QFileDialog>
--- src/software/ui/SfM/control_points_registration/main.cpp
+++ src/software/ui/SfM/control_points_registration/main.cpp
@@ -7,7 +7,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <QApplication>
-#include "mainLayout.hpp"
+#include "MainWindow.hpp"
 
 int main(int argc, char ** argv)
 {
