584d00cd9307513d3de5a25df0213de59200ffa7
Yan <KevinYan37@163.com>
2018-03-02 13:37:47 +0800

    add contributor

--- src/software/SfM/main_SfMInit_ImageListingFromKnownPoses.cpp
+++ src/software/SfM/main_SfMInit_ImageListingFromKnownPoses.cpp
@@ -1,6 +1,6 @@
 // This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
 
-// Copyright (c) 2018 Yan Qingsong.
+// Copyright (c) 2018 Yan Qingsong,Pierre MOULON.
 
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
