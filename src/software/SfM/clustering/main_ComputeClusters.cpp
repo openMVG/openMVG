d7b1db7facd212b313b6d63cb6390d50d50d4986
pmoulon <pmoulon@gmail.com>
2020-03-20 08:44:48 -0700

    [build] Remove useless includes

--- src/software/SfM/clustering/main_ComputeClusters.cpp
+++ src/software/SfM/clustering/main_ComputeClusters.cpp
@@ -6,7 +6,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#include "openMVG/image/image_io.hpp"
 #include "openMVG/sfm/sfm.hpp"
 #include "openMVG/system/timer.hpp"
 #include "third_party/cmdLine/cmdLine.h"
@@ -30,7 +29,6 @@
 using namespace openMVG;
 using namespace openMVG::cameras;
 using namespace openMVG::geometry;
-using namespace openMVG::image;
 using namespace openMVG::sfm;
 
 /**
