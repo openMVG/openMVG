cd3ee2b75b879cd26e90a1fbaf528e3138a71199
Marc Eder <6818607+meder411@users.noreply.github.com>
2020-06-05 11:30:56 -0400

    cleaned up tangent image code a bit

--- src/openMVG/spherical/spherical_test.cpp
+++ src/openMVG/spherical/spherical_test.cpp
@@ -1,36 +1,49 @@
-#include "openMVG/image/image_io.hpp"
 #include "openMVG/spherical/spherical.hpp"
+#include "openMVG/image/image_io.hpp"
 
 #include "testing/testing.h"
 
 #include <sstream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace openMVG;
 using namespace openMVG::image;
 using namespace openMVG::spherical;
 
-TEST(Spherical,CreateTangentImages)
-{
+TEST(Spherical, EquirectToTangent) {
   Image<RGBColor> image;
-  const std::string png_filename = std::string(THIS_SOURCE_DIR) + "/earthmap4k.jpg";
+  const std::string png_filename =
+      std::string(THIS_SOURCE_DIR) + "/earthmap4k.jpg";
   std::cout << png_filename << std::endl;
   EXPECT_TRUE(ReadImage(png_filename.c_str(), &image));
 
-  const std::string test_filename = "earthmap4k.jpg";
-  EXPECT_TRUE(WriteImage(test_filename.c_str(), image));
-
   std::vector<Image<RGBColor>> tangent_images;
-  Equirectangular2Tangent(image, 9L, 0L, tangent_images);
+  CreateTangentImages(image, 9L, 0L, tangent_images);
 
   for (size_t i = 0; i < tangent_images.size(); i++) {
-    const std::string out_filename = ("test_tangent images_" + std::to_string(i) + ".png");
-    EXPECT_TRUE( WriteImage( out_filename.c_str(), tangent_images[i]) );
+    const std::string out_filename =
+        ("test_tangent images_" + std::to_string(i) + ".png");
+    EXPECT_TRUE(WriteImage(out_filename.c_str(), tangent_images[i]));
   }
 }
 
+TEST(Spherical, TangentToEquirect) {
+  Image<RGBColor> image;
+  const std::string png_filename =
+      std::string(THIS_SOURCE_DIR) + "/earthmap4k.jpg";
+  std::cout << png_filename << std::endl;
+  EXPECT_TRUE(ReadImage(png_filename.c_str(), &image));
+
+  std::vector<Image<RGBColor>> tangent_images;
+  CreateTangentImages(image, 9L, 0L, tangent_images);
+
+  // TangentToEquirectangular(tangent_images, )
+}
 
 /* ************************************************************************* */
-int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+int main() {
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
 /* ************************************************************************* */
