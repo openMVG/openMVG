e4f1e85e3bf43a51e423b39b370b1dcd52d8d849
Ricardo Fabbri <rfabbri@gmail.com>
2020-06-16 17:02:39 -0300

    modularize sample to class

--- src/openMVG_Samples/multiview_trifocal/trifocal.cpp
+++ src/openMVG_Samples/multiview_trifocal/trifocal.cpp
@@ -148,27 +148,188 @@ class ThreeViewKernel {
     const Mat &x1_, &x2_, &x3_; // corresponding point of the trifical configuration
 };
 
+
+struct TrifocalSampleApp {
+ public:
+  void ExtractKeypoints() {
+     // Call Keypoint extractor
+     using namespace openMVG::features;
+     std::unique_ptr<Image_describer> image_describer;
+     image_describer.reset(new SIFT_Anatomy_Image_describer(SIFT_Anatomy_Image_describer::Params()));
+
+     if (!image_describer)
+     {
+       std::cerr << "Invalid Image_describer type" << std::endl;
+       return EXIT_FAILURE;
+     }
+  }
+
+  void MatchKeypoints() {
+    //--
+    // Compute corresponding points {{0,1}, {1,2}}
+    //--
+    //-- Perform matching -> find Nearest neighbor, filtered with Distance ratio
+    //std::unique_ptr<Matcher> collectionMatcher(new Cascade_Hashing_Matcher_Regions(fDistRatio));
+    //collectionMatcher->Match(regions_provider, {{0,1}, {1,2}}, pairwise_matches, &progress);
+    matching::PairWiseMatches pairwise_matches;
+    matching::DistanceRatioMatch(
+      0.8, matching::BRUTE_FORCE_L2,
+      * regions_per_image.at(0).get(),
+      * regions_per_image.at(1).get(),
+      pairwise_matches[{0,1}]);
+    matching::DistanceRatioMatch(
+      0.8, matching::BRUTE_FORCE_L2,
+      * regions_per_image.at(1).get(),
+      * regions_per_image.at(2).get(),
+      pairwise_matches[{1,2}]);
+  }
+
+  void ComputeTracks() {
+      //
+      // Computing tracks
+      //
+      openMVG::tracks::STLMAPTracks tracks;
+      openMVG::tracks::TracksBuilder track_builder;
+      track_builder.Build(pairwise_matches);
+      track_builder.Filter(3);
+      track_builder.ExportToSTL(tracks);
+  }
+
+  void Stats() {
+      // Display some statistics
+      std::cout
+        <<  regions_per_image.at(0)->RegionCount() << " #Features on image A" << std::endl
+        <<  regions_per_image.at(1)->RegionCount() << " #Features on image B" << std::endl
+        <<  regions_per_image.at(2)->RegionCount() << " #Features on image C" << std::endl
+        << pairwise_matches.at({0,1}).size() << " #matches with Distance Ratio filter" << std::endl
+        << pairwise_matches.at({1,2}).size() << " #matches with Distance Ratio filter" << std::endl
+        << tracks.size() << " #tracks" << std::endl;
+
+      const std::array<const SIFT_Regions*, 3> sio_regions = {
+        dynamic_cast<SIFT_Regions*>(regions_per_image.at(0).get()),
+        dynamic_cast<SIFT_Regions*>(regions_per_image.at(1).get()),
+        dynamic_cast<SIFT_Regions*>(regions_per_image.at(2).get())
+      };
+  }
+
+  void ExtractXYOrientation() {
+      //
+      // Build datum (corresponding {x,y,orientation})
+      //
+      std::array<Mat, 3> datum;              
+      datum[0].resize(4, tracks.size());
+      datum[1].resize(4, tracks.size());
+      datum[2].resize(4, tracks.size());
+      int idx = 0;
+      for (const auto &track_it: tracks)
+      {
+        auto iter = track_it.second.cbegin();
+        const uint32_t
+          i = iter->second,
+          j = (++iter)->second,
+          k = (++iter)->second;
+        //
+        const auto feature_i = sio_regions[0]->Features()[i];
+        const auto feature_j = sio_regions[1]->Features()[j];
+        const auto feature_k = sio_regions[2]->Features()[k];
+        datum[0].col(idx) << feature_i.x(), feature_i.y(), std::cos(feature_i.orientation()), std::sin(feature_i.orientation());
+        // datum[0].col(idx) << feature_i.x(), feature_i.y(), feature_i.orientation();
+        datum[1].col(idx) << feature_j.x(), feature_j.y(), std::cos(feature_i.orientation()), std::sin(feature_i.orientation());
+    feature_j.orientation();
+        datum[2].col(idx) << feature_k.x(), feature_k.y(), std::cos(feature_i.orientation()), std::sin(feature_i.orientation());
+        
+        //Gabriel:Calling both K invertions:
+        //
+        std::cout << invert_intrinsics(K, datum[0].col(idx).data(), datum[0].col(idx).data(), tracks.size());
+        // std::cout << invert_intrinsics_tgt((double *)[3][3](K.data()), datum[0].col(idx).data()+2, datum[0].col(idx).data()+2, tracks.size());
+        ++idx;
+      }
+      std::cout <<  datum[0] << "blyat" << std::endl;
+  }
+
+  void Display() {
+    //
+    // Display demo
+    //
+    {
+      const int svg_w = images[0].Width();
+      const int svg_h = images[0].Height() + images[1].Height() + images[2].Height();
+      svg::svgDrawer svg_stream(svg_w, svg_h);
+
+      // Draw image side by side
+      svg_stream.drawImage(image_filenames[0], images[0].Width(), images[0].Height());
+      svg_stream.drawImage(image_filenames[1], images[1].Width(), images[1].Height(), 0, images[0].Height());
+      svg_stream.drawImage(image_filenames[2], images[2].Width(), images[2].Height(), 0, images[0].Height() + images[1].Height());
+
+      for (const auto &track_it: tracks)
+      {
+        auto iter = track_it.second.cbegin();
+        const uint32_t
+          i = iter->second,
+          j = (++iter)->second,
+          k = (++iter)->second;
+        //
+        const auto feature_i = sio_regions[0]->Features()[i];
+        const auto feature_j = sio_regions[1]->Features()[j];
+        const auto feature_k = sio_regions[2]->Features()[k];
+
+        svg_stream.drawCircle(
+          feature_i.x(), feature_i.y(), feature_i.scale(),
+          svg::svgStyle().stroke("yellow", 1));
+        svg_stream.drawCircle(
+          feature_j.x(), feature_j.y() + images[0].Height(), feature_k.scale(),
+          svg::svgStyle().stroke("yellow", 1));
+        svg_stream.drawCircle(
+          feature_k.x(), feature_j.y() + images[0].Height() + images[1].Height(), feature_k.scale(),
+          svg::svgStyle().stroke("yellow", 1));
+        svg_stream.drawLine(
+          feature_i.x(), feature_i.y(),
+          feature_j.x(), feature_j.y() + images[0].Height(),
+          svg::svgStyle().stroke("blue", 1));
+        svg_stream.drawLine(
+          feature_j.x(), feature_j.y() + images[0].Height(),
+          feature_k.x(), feature_j.y() + images[0].Height() + images[1].Height(),
+          svg::svgStyle().stroke("blue", 1));
+      }
+      std::ofstream svg_file( "trifocal_track.svg" );
+      if (svg_file.is_open())
+      {
+        svg_file << svg_stream.closeSvgFile().str();
+      }
+    }
+  }
+
+  void RobustSolve() {
+    using TrifocalKernel = ThreeViewKernel<Trifocal3PointPositionTangentialSolver, Trifocal3PointPositionTangentialSolver>;
+    const TrifocalKernel trifocal_kernel(datum[0], datum[1], datum[2]);
+
+    const double threshold_pix = 2.0;
+    std::vector<uint32_t> vec_inliers;
+    const auto model = MaxConsensus(trifocal_kernel, ScorerEvaluator<TrifocalKernel>(threshold_pix), &vec_inliers);
+  }
+
+  // 3x3 intrinsic matrix for this default test case
+  // This representation is specific for fast non-homog action
+  // Just eliminate last row 
+  //
+  // This matrix is calib.intrinsic for the synthcurves spherical dataset
+  double K_[2][3] = {  // some default value for testing
+    {2584.9325098195013197, 0, 249.77137587221417903},
+    {0, 2584.7918606057692159, 278.31267937919352562}
+   //  0 0 1 
+  };
+}
+
 int main(int argc, char **argv) {
 
   // The three images used to compute the trifocal configuration
   std::array<std::string, 3> image_filenames;
-  const Mat intrinsics_filename;
+  std::array<std::string> intrinsics_filename;
   CmdLine cmd;
   cmd.add( make_option('a', image_filenames[0], "image_a") );
   cmd.add( make_option('b', image_filenames[1], "image_b") );
   cmd.add( make_option('c', image_filenames[2], "image_c") );
-  // cmd.add( make_option('k', intrinsics_filename, "K") );
-// 3x3 intrinsic matrix for this default test case
-// This representation is specific for fast non-homog action
-// Just eliminate last row 
-//
-// This matrix is calib.intrinsic for the synthcurves spherical dataset
-static double K[2][3] = {
-  {2584.9325098195013197, 0, 249.77137587221417903},
-  {0, 2584.7918606057692159, 278.31267937919352562}
- //  0 0 1 
-};
-
+  cmd.add( make_option('K', intrinsics_filename, "K matrix") );
 
   try {
       if (argc == 1) throw std::string("Invalid command line parameter.");
@@ -181,159 +342,17 @@ static double K[2][3] = {
       return EXIT_FAILURE;
   }
 
-  // Call Keypoint extractor
-  using namespace openMVG::features;
-  std::unique_ptr<Image_describer> image_describer;
-  image_describer.reset(new SIFT_Anatomy_Image_describer(SIFT_Anatomy_Image_describer::Params()));
+  TrifocalSampleApp T;
 
-  if (!image_describer)
-  {
-    std::cerr << "Invalid Image_describer type" << std::endl;
-    return EXIT_FAILURE;
-  }
+  T.ProcessCMDLine();
 
-  std::array<Image<unsigned char>, 3> images;
-  std::map<IndexT, std::unique_ptr<features::Regions>> regions_per_image;
-  for (const int image_idx : {0,1,2})
-  {
-    if (ReadImage(image_filenames[image_idx].c_str(), &images[image_idx]))
-      image_describer->Describe(images[image_idx], regions_per_image[image_idx]);
-  }
+  T.ExtractKeypoints();
+  T.MatchKeypoints();
+  T.Stats();
+  T.ExtractXYOrientation();
+  T.Display();
+  T.RobustSolve();
 
-  //--
-  // Compute corresponding points {{0,1}, {1,2}}
-  //--
-  //-- Perform matching -> find Nearest neighbor, filtered with Distance ratio
-  //std::unique_ptr<Matcher> collectionMatcher(new Cascade_Hashing_Matcher_Regions(fDistRatio));
-  //collectionMatcher->Match(regions_provider, {{0,1}, {1,2}}, pairwise_matches, &progress);
-  matching::PairWiseMatches pairwise_matches;
-  matching::DistanceRatioMatch(
-    0.8, matching::BRUTE_FORCE_L2,
-    * regions_per_image.at(0).get(),
-    * regions_per_image.at(1).get(),
-    pairwise_matches[{0,1}]);
-  matching::DistanceRatioMatch(
-    0.8, matching::BRUTE_FORCE_L2,
-    * regions_per_image.at(1).get(),
-    * regions_per_image.at(2).get(),
-    pairwise_matches[{1,2}]);
-
-  //
-  // Computing tracks
-  //
-  openMVG::tracks::STLMAPTracks tracks;
-  openMVG::tracks::TracksBuilder track_builder;
-  track_builder.Build(pairwise_matches);
-  track_builder.Filter(3);
-  track_builder.ExportToSTL(tracks);
-
-  // Display some statistics
-  std::cout
-    <<  regions_per_image.at(0)->RegionCount() << " #Features on image A" << std::endl
-    <<  regions_per_image.at(1)->RegionCount() << " #Features on image B" << std::endl
-    <<  regions_per_image.at(2)->RegionCount() << " #Features on image C" << std::endl
-    << pairwise_matches.at({0,1}).size() << " #matches with Distance Ratio filter" << std::endl
-    << pairwise_matches.at({1,2}).size() << " #matches with Distance Ratio filter" << std::endl
-    << tracks.size() << " #tracks" << std::endl;
-
-  const std::array<const SIFT_Regions*, 3> sio_regions = {
-    dynamic_cast<SIFT_Regions*>(regions_per_image.at(0).get()),
-    dynamic_cast<SIFT_Regions*>(regions_per_image.at(1).get()),
-    dynamic_cast<SIFT_Regions*>(regions_per_image.at(2).get())
-  };
-
-  //
-  // Build datum (corresponding {x,y,orientation})
-  //
-  std::array<Mat, 3> datum;              
-  datum[0].resize(4, tracks.size());
-  datum[1].resize(4, tracks.size());
-  datum[2].resize(4, tracks.size());
-  int idx = 0;
-  for (const auto &track_it: tracks)
-  {
-    auto iter = track_it.second.cbegin();
-    const uint32_t
-      i = iter->second,
-      j = (++iter)->second,
-      k = (++iter)->second;
-    //
-    const auto feature_i = sio_regions[0]->Features()[i];
-    const auto feature_j = sio_regions[1]->Features()[j];
-    const auto feature_k = sio_regions[2]->Features()[k];
-    datum[0].col(idx) << feature_i.x(), feature_i.y(), std::cos(feature_i.orientation()), std::sin(feature_i.orientation());
-    // datum[0].col(idx) << feature_i.x(), feature_i.y(), feature_i.orientation();
-    datum[1].col(idx) << feature_j.x(), feature_j.y(), std::cos(feature_i.orientation()), std::sin(feature_i.orientation());
-feature_j.orientation();
-    datum[2].col(idx) << feature_k.x(), feature_k.y(), std::cos(feature_i.orientation()), std::sin(feature_i.orientation());
-    
-    //Gabriel:Calling both K invertions:
-    //
-    std::cout << invert_intrinsics(K, datum[0].col(idx).data(), datum[0].col(idx).data(), tracks.size());
-    // std::cout << invert_intrinsics_tgt((double *)[3][3](K.data()), datum[0].col(idx).data()+2, datum[0].col(idx).data()+2, tracks.size());
-   
-    ++idx;
-  }
-std::cout <<  datum[0] << "blyat" << std::endl;
-  //
-  // Display demo
-  //
-  {
-    const int svg_w = images[0].Width();
-    const int svg_h = images[0].Height() + images[1].Height() + images[2].Height();
-    svg::svgDrawer svg_stream(svg_w, svg_h);
-
-    // Draw image side by side
-    svg_stream.drawImage(image_filenames[0], images[0].Width(), images[0].Height());
-    svg_stream.drawImage(image_filenames[1], images[1].Width(), images[1].Height(), 0, images[0].Height());
-    svg_stream.drawImage(image_filenames[2], images[2].Width(), images[2].Height(), 0, images[0].Height() + images[1].Height());
-
-    for (const auto &track_it: tracks)
-    {
-      auto iter = track_it.second.cbegin();
-      const uint32_t
-        i = iter->second,
-        j = (++iter)->second,
-        k = (++iter)->second;
-      //
-      const auto feature_i = sio_regions[0]->Features()[i];
-      const auto feature_j = sio_regions[1]->Features()[j];
-      const auto feature_k = sio_regions[2]->Features()[k];
-
-      svg_stream.drawCircle(
-        feature_i.x(), feature_i.y(), feature_i.scale(),
-        svg::svgStyle().stroke("yellow", 1));
-      svg_stream.drawCircle(
-        feature_j.x(), feature_j.y() + images[0].Height(), feature_k.scale(),
-        svg::svgStyle().stroke("yellow", 1));
-      svg_stream.drawCircle(
-        feature_k.x(), feature_j.y() + images[0].Height() + images[1].Height(), feature_k.scale(),
-        svg::svgStyle().stroke("yellow", 1));
-      svg_stream.drawLine(
-        feature_i.x(), feature_i.y(),
-        feature_j.x(), feature_j.y() + images[0].Height(),
-        svg::svgStyle().stroke("blue", 1));
-      svg_stream.drawLine(
-        feature_j.x(), feature_j.y() + images[0].Height(),
-        feature_k.x(), feature_j.y() + images[0].Height() + images[1].Height(),
-        svg::svgStyle().stroke("blue", 1));
-    }
-    std::ofstream svg_file( "trifocal_track.svg" );
-    if (svg_file.is_open())
-    {
-      svg_file << svg_stream.closeSvgFile().str();
-    }
-  }
-
-  //
-  // Robust solving
-  //
-  using TrifocalKernel = ThreeViewKernel<Trifocal3PointPositionTangentialSolver, Trifocal3PointPositionTangentialSolver>;
-  const TrifocalKernel trifocal_kernel(datum[0], datum[1], datum[2]);
-
-  const double threshold_pix = 2.0;
-  std::vector<uint32_t> vec_inliers;
-  const auto model = MaxConsensus(trifocal_kernel, ScorerEvaluator<TrifocalKernel>(threshold_pix), &vec_inliers);
   
   // We can then display the inlier and the 3D camera configuration as PLY
 
