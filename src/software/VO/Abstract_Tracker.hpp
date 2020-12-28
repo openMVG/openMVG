59079612e9c4599d9652ebf6fc8e3a63eb33e840
pmoulon <pmoulon@gmail.com>
2020-12-28 14:17:11 -0800

    [Build] Fix clang warnings. #1799

--- src/software/VO/Abstract_Tracker.hpp
+++ src/software/VO/Abstract_Tracker.hpp
@@ -17,6 +17,11 @@ struct Abstract_Tracker
 {
   Abstract_Tracker() = default;
 
+protected:
+  ~Abstract_Tracker() = default;
+
+public:
+
   // Try to track the pt_to_track features
   virtual bool track
   (
