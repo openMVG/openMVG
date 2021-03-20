a9972e1d75abf1cf727c3630ef8cb2eb2083f4ba
pmoulon <pmoulon@gmail.com>
2021-03-19 23:31:11 -0700

    [openmvg/features/sift] Sift patent has expired #1759

--- src/openMVG/features/sift/sift_DescriptorExtractor.hpp
+++ src/openMVG/features/sift/sift_DescriptorExtractor.hpp
@@ -7,29 +7,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 /*
-
-== Patent Warning and License =================================================
-
-The SIFT method is patented
-
-    [2] "Method and apparatus for identifying scale invariant features
-      in an image."
-        David G. Lowe
-        Patent number: 6711293
-        Filing date: Mar 6, 2000
-        Issue date: Mar 23, 2004
-        Application number: 09/519,89
-
- These source codes are made available for the exclusive aim of serving as
- scientific tool to verify the soundness and completeness of the algorithm
- description. Compilation, execution and redistribution of this file may
- violate patents rights in certain countries. The situation being different
- for every country and changing over time, it is your responsibility to
- determine which patent rights restrictions apply to you before you compile,
- use, modify, or redistribute this file. A patent lawyer is qualified to make
- this determination. If and only if they don't conflict with any patent terms,
- you can benefit from the following license terms attached to this file.
-
 The implementation is based on
 
     [1] "Anatomy of the SIFT Method."
@@ -105,7 +82,7 @@ public:
   */
   void operator()
   (
-    const Octave & octave ,
+    const Octave & octave,
     std::vector<Keypoint> & keypoints
   )
   {
@@ -208,7 +185,7 @@ protected:
     const int siMax = std::min((int)(x + R + 0.5), w-1);
     const int sjMax = std::min((int)(y + R + 0.5), h-1);
 
-    // For each pixel neighbourhood patch
+    // For each pixel neighborhood patch
     for (int si = siMin; si <= siMax; ++si)
     {
       for (int sj = sjMin; sj <= sjMax; ++sj)
