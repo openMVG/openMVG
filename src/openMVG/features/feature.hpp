openMVG/openMVG

commit 88c228d5e8d11b016b85d72469eb8c714510299b

Code cleaning (explicit, const iterators, refactoring) #1626

src/openMVG/features/feature.hpp
@@ -172,7 +172,7 @@ static bool saveFeatsToFile(
   std::ofstream file(sfileNameFeats.c_str());
   if (!file.is_open())
     return false;
-  std::copy(vec_feat.begin(), vec_feat.end(),
+  std::copy(vec_feat.cbegin(), vec_feat.cend(),
             std::ostream_iterator<typename FeaturesT::value_type >(file,"\n"));
   const bool bOk = file.good();
   file.close();
@@ -189,10 +189,8 @@ void PointsToMat(
   using ValueT = typename FeaturesT::value_type; // Container type
 
   size_t i = 0;
-  for (typename FeaturesT::const_iterator iter = vec_feats.begin();
-    iter != vec_feats.end(); ++iter, ++i)
+  for (const auto &feat : vec_feats)
   {
-    const ValueT & feat = *iter;
     m.col(i) << feat.x(), feat.y();
   }
 }

src/openMVG/features/descriptor.hpp
@@ -169,7 +169,7 @@ inline bool saveDescsToFile(
   std::ofstream file(sfileNameDescs.c_str());
   if (!file.is_open())
     return false;
-  std::copy(vec_desc.begin(), vec_desc.end(),
+  std::copy(vec_desc.cbegin(), vec_desc.cend(),
             std::ostream_iterator<typename DescriptorsT::value_type >(file,"\n"));
   const bool bOk = file.good();
   file.close();
@@ -216,9 +216,9 @@ inline bool saveDescsToBinFile(
   //Write the number of descriptor
   const std::size_t cardDesc = vec_desc.size();
   file.write((const char*) &cardDesc,  sizeof(std::size_t));
-  for (typename DescriptorsT::const_iterator iter = vec_desc.begin();
-    iter != vec_desc.end(); ++iter) {
-    file.write((const char*) (*iter).data(),
+  //Write descriptor content
+  for (const auto iter : vec_desc) {
+    file.write((const char*) iter.data(),
       VALUE::static_size*sizeof(typename VALUE::bin_type));
   }
   const bool bOk = file.good();

src/openMVG/matching/indMatch.hpp
@@ -24,14 +24,14 @@ namespace matching {
 /// A sort operator exist in order to remove duplicates of IndMatch series.
 struct IndMatch
 {
-  IndMatch(IndexT i = 0, IndexT j = 0) : i_(i), j_(j)  {}
+  IndMatch(const IndexT i = 0, const IndexT j = 0) : i_(i), j_(j)  {}
 
   /// Remove duplicates ((i_, j_) that appears multiple times)
   static bool getDeduplicated(std::vector<IndMatch> & vec_match)  {
 
     const size_t sizeBefore = vec_match.size();
-    const std::set<IndMatch> set_deduplicated( vec_match.begin(), vec_match.end());
-    vec_match.assign(set_deduplicated.begin(), set_deduplicated.end());
+    const std::set<IndMatch> set_deduplicated( vec_match.cbegin(), vec_match.cend());
+    vec_match.assign(set_deduplicated.cbegin(), set_deduplicated.cend());
     return sizeBefore != vec_match.size();
   }
 

src/openMVG/matching/indMatchDecoratorXY.hpp
@@ -28,7 +28,7 @@ class IndMatchDecorator
 {
   struct IndMatchDecoratorStruct
   {
-    IndMatchDecoratorStruct
+    explicit IndMatchDecoratorStruct
     (
       T xa, T ya,
       T xb, T yb,
@@ -64,7 +64,6 @@ class IndMatchDecorator
       const IndMatchDecoratorStruct& m2
     )
     {
-
       return (m1.x1==m2.x1 && m1.y1==m2.y1 &&
         m1.x2==m2.x2 && m1.y2==m2.y2);
     }
@@ -79,11 +78,12 @@ class IndMatchDecorator
   };
 public:
 
-  IndMatchDecorator
+  template<typename Feature_t>
+  explicit IndMatchDecorator
   (
     const std::vector<IndMatch> & vec_matches,
-    const std::vector<features::SIOPointFeature> & leftFeat,
-    const std::vector<features::SIOPointFeature> & rightFeat
+    const std::vector<Feature_t> & leftFeat,
+    const std::vector<Feature_t> & rightFeat
   )
   :vec_matches_(vec_matches)
   {
@@ -91,31 +91,13 @@ public:
     {
       const size_t I = cur_vec_match.i_;
       const size_t J = cur_vec_match.j_;
-      vecDecoredMatches_.push_back(
-        IndMatchDecoratorStruct(leftFeat[I].x(),leftFeat[I].y(),
-        rightFeat[J].x(), rightFeat[J].y(), cur_vec_match));
+      vec_decoredMatches_.emplace_back(
+        leftFeat[I].x(),leftFeat[I].y(),
+        rightFeat[J].x(), rightFeat[J].y(), cur_vec_match);
     }
   }
 
-  IndMatchDecorator
-  (
-    const std::vector<IndMatch> & vec_matches,
-    const std::vector<features::PointFeature> & leftFeat,
-    const std::vector<features::PointFeature> & rightFeat
-  )
-  :vec_matches_(vec_matches)
-  {
-    for ( const auto & cur_vec_match : vec_matches )
-    {
-      const size_t I = cur_vec_match.i_;
-      const size_t J = cur_vec_match.j_;
-      vecDecoredMatches_.push_back(
-        IndMatchDecoratorStruct(leftFeat[I].x(),leftFeat[I].y(),
-        rightFeat[J].x(), rightFeat[J].y(), cur_vec_match));
-    }
-  }
-
-  IndMatchDecorator
+  explicit IndMatchDecorator
   (
     const std::vector<IndMatch> & vec_matches,
     const Mat & leftFeat,
@@ -127,23 +109,23 @@ public:
     {
       const size_t I = cur_vec_match.i_;
       const size_t J = cur_vec_match.j_;
-      vecDecoredMatches_.push_back(
-        IndMatchDecoratorStruct(leftFeat.col(I)(0),leftFeat.col(I)(1),
-        rightFeat.col(J)(0), rightFeat.col(J)(1), cur_vec_match));
+      vec_decoredMatches_.emplace_back(
+        leftFeat.col(I)(0),leftFeat.col(I)(1),
+        rightFeat.col(J)(0), rightFeat.col(J)(1), cur_vec_match);
     }
   }
 
   /// Remove duplicates (same (x1,y1) coords that appears multiple times)
   size_t getDeduplicated(std::vector<IndMatch> & vec_matches)
   {
-    const size_t sizeBefore = vecDecoredMatches_.size();
-    std::set<IndMatchDecoratorStruct> set_deduplicated(
-      vecDecoredMatches_.begin(),vecDecoredMatches_.end());
-    vecDecoredMatches_.assign(set_deduplicated.begin(), set_deduplicated.end());
+    const size_t sizeBefore = vec_decoredMatches_.size();
+    const std::set<IndMatchDecoratorStruct> set_deduplicated(
+      vec_decoredMatches_.cbegin(),vec_decoredMatches_.cend());
+    vec_decoredMatches_.assign(set_deduplicated.cbegin(), set_deduplicated.cend());
 
-    vec_matches.resize(vecDecoredMatches_.size());
-    for (size_t i = 0; i < vecDecoredMatches_.size(); ++i)  {
-      const IndMatch & idxM = vecDecoredMatches_[i].index;
+    vec_matches.resize(vec_decoredMatches_.size());
+    for (size_t i = 0; i < vec_decoredMatches_.size(); ++i)  {
+      const IndMatch & idxM = vec_decoredMatches_[i].index;
       vec_matches[i] = idxM;
     }
 
@@ -160,7 +142,7 @@ public:
   {
     std::ofstream f(nameFile);
     if (f.is_open() ) {
-      std::copy(vecDecoredMatches_.begin(), vecDecoredMatches_.end(),
+      std::copy(vec_decoredMatches_.cbegin(), vec_decoredMatches_.cend(),
         std::ostream_iterator<IndMatchDecoratorStruct>(f, ""));
     }
     return f.is_open();
@@ -168,7 +150,7 @@ public:
 
 private:
   std::vector<IndMatch> vec_matches_;
-  std::vector<IndMatchDecoratorStruct> vecDecoredMatches_;
+  std::vector<IndMatchDecoratorStruct> vec_decoredMatches_;
 };
 
 }  // namespace matching

src/openMVG/robust_estimation/robust_estimator_ACRansac_test.cpp
@@ -243,7 +243,7 @@ void generateLine(Mat & points, size_t nbPoints, int W, int H, float noise, floa
 // Structure used to avoid repetition in a given series
 struct IndMatchd
 {
-  IndMatchd(double i = 0, double j = 0): i_(i), j_(j)
+  explicit IndMatchd(double i = 0.0, double j = 0.0): i_(i), j_(j)
   {}
 
   double i_, j_;

commit 15197eb34a4f58ee22a0b5d4be9c6924b097beb4

[features] Fix regression
Fix regression introduced by https://github.com/openMVG/openMVG/commit/88c228d5e8d11b016b85d72469eb8c714510299b#diff-06fed17aac89d224546dcdc588f65b02abe60af50eeed1cb3d373b6bcb06c5ecR192

src/openMVG/features/feature.hpp
@@ -190,7 +190,7 @@ void PointsToMat(
   size_t i = 0;
   for (const auto &feat : vec_feats)
   {
-    m.col(i) << feat.x(), feat.y();
+    m.col(i++) << feat.x(), feat.y();
   }
 }
 