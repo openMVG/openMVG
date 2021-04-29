3d966b64037c2a390f1db8b7c06532507557d4fd
yohann.cabon <yohann.cabon@naverlabs.com>
2021-04-29 16:25:35 +0200

    add script to convert matches between txt <-> bin

---
+++ src/software/SfM/main_convertMatches.cpp
@@ -0,0 +1,114 @@
+// This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
+
+// Copyright (c) 2012, 2013, 2015 Pierre MOULON.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "openMVG/matching/indMatch.hpp"
+#include "openMVG/matching/indMatch_utils.hpp"
+#include "openMVG/matching/svg_matches.hpp"
+#include "openMVG/image/image_io.hpp"
+#include "openMVG/sfm/pipelines/sfm_features_provider.hpp"
+#include "openMVG/sfm/pipelines/sfm_matches_provider.hpp"
+#include "openMVG/sfm/sfm_data.hpp"
+#include "openMVG/sfm/sfm_data_io.hpp"
+
+#include "third_party/cmdLine/cmdLine.h"
+#include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
+#include "third_party/progress/progress_display.hpp"
+
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <map>
+
+using namespace openMVG;
+using namespace openMVG::features;
+using namespace openMVG::matching;
+using namespace openMVG::sfm;
+
+int main(int argc, char ** argv)
+{
+  CmdLine cmd;
+
+  std::string sSfM_Data_Filename;
+  std::string sMatchesDir;
+  std::string sMatchFile;
+  std::string sOutMatchFile = "";
+
+  cmd.add( make_option('i', sSfM_Data_Filename, "input_file") );
+  cmd.add( make_option('d', sMatchesDir, "matchdir") );
+  cmd.add( make_option('m', sMatchFile, "matchfile") );
+  cmd.add( make_option('o', sOutMatchFile, "outmatchfile") );
+
+  try {
+      if (argc == 1) throw std::string("Invalid command line parameter.");
+      cmd.process(argc, argv);
+  } catch (const std::string& s) {
+      std::cerr << "Export matches to text.\nUsage: " << argv[0] << "\n"
+      << "[-i|--input_file file] path to a SfM_Data scene\n"
+      << "[-d|--matchdir path]\n"
+      << "[-m|--sMatchFile filename]\n"
+      << "[-o|--outmatchfile filename]\n"
+      << std::endl;
+
+      std::cerr << s << std::endl;
+      return EXIT_FAILURE;
+  }
+
+  if (sMatchesDir.empty()) {
+    std::cerr << "\nmatchdir cannot be an empty option" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (sMatchFile.empty()) {
+    std::cerr << "\nmatchfile cannot be an empty option" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (sOutMatchFile.empty())  {
+    std::cerr << "\noutmatchfile cannot be an empty option" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //---------------------------------------
+  // Read SfM Scene (image view names)
+  //---------------------------------------
+  SfM_Data sfm_data;
+  if (!Load(sfm_data, sSfM_Data_Filename, ESfM_Data(VIEWS|INTRINSICS))) {
+    std::cerr << std::endl
+      << "The input SfM_Data file \""<< sSfM_Data_Filename << "\" cannot be read." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  //---------------------------------------
+  // Load SfM Scene regions
+  //---------------------------------------
+  // Init the regions_type from the image describer file (used for image regions extraction)
+  const std::string sImage_describer = stlplus::create_filespec(sMatchesDir, "image_describer", "json");
+  std::unique_ptr<Regions> regions_type = Init_region_type_from_file(sImage_describer);
+  if (!regions_type)
+  {
+    std::cerr << "Invalid: "
+      << sImage_describer << " regions type file." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Read the matches
+  std::shared_ptr<Matches_Provider> matches_provider = std::make_shared<Matches_Provider>();
+  if (!matches_provider->load(sfm_data, sMatchFile)) {
+    std::cerr << "\nInvalid matches file." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (!Save(matches_provider->pairWise_matches_, std::string(sOutMatchFile)))
+  {
+    std::cerr
+        << "Cannot save computed matches in: "
+        << sMatchFile;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
