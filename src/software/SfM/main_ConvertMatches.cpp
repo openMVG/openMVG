47cc6e7e65c4f8bfb29ed2aa0129ab3561eef560
yohann.cabon <yohann.cabon@naverlabs.com>
2021-05-07 11:10:54 +0200

    trim down code

--- src/software/SfM/main_ConvertMatches.cpp
+++ src/software/SfM/main_ConvertMatches.cpp
@@ -8,36 +8,22 @@
 
 #include "openMVG/matching/indMatch.hpp"
 #include "openMVG/matching/indMatch_utils.hpp"
-#include "openMVG/matching/svg_matches.hpp"
-#include "openMVG/image/image_io.hpp"
-#include "openMVG/sfm/pipelines/sfm_matches_provider.hpp"
-#include "openMVG/sfm/sfm_data.hpp"
-#include "openMVG/sfm/sfm_data_io.hpp"
 
 #include "third_party/cmdLine/cmdLine.h"
-#include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
-#include "third_party/progress/progress_display.hpp"
 
 #include <cstdlib>
 #include <string>
-#include <vector>
-#include <fstream>
-#include <map>
 
 using namespace openMVG;
-using namespace openMVG::features;
 using namespace openMVG::matching;
-using namespace openMVG::sfm;
 
 int main(int argc, char ** argv)
 {
   CmdLine cmd;
 
-  std::string sSfM_Data_Filename;
   std::string sMatchFile;
-  std::string sOutMatchFile = "";
+  std::string sOutMatchFile;
 
-  cmd.add( make_option('i', sSfM_Data_Filename, "input_file") );
   cmd.add( make_option('m', sMatchFile, "matchfile") );
   cmd.add( make_option('o', sOutMatchFile, "outmatchfile") );
 
@@ -46,7 +32,6 @@ int main(int argc, char ** argv)
       cmd.process(argc, argv);
   } catch (const std::string& s) {
       std::cerr << "Convert matches from bin to txt or txt to bin.\nUsage: " << argv[0] << "\n"
-      << "[-i|--input_file file] path to a SfM_Data scene\n"
       << "[-m|--sMatchFile filename]\n"
       << "[-o|--outmatchfile filename]\n"
       << std::endl;
@@ -64,25 +49,15 @@ int main(int argc, char ** argv)
     return EXIT_FAILURE;
   }
 
-  //---------------------------------------
-  // Read SfM Scene (image view names)
-  //---------------------------------------
-  SfM_Data sfm_data;
-  if (!Load(sfm_data, sSfM_Data_Filename, ESfM_Data(VIEWS|INTRINSICS))) {
-    std::cerr << std::endl
-      << "The input SfM_Data file \""<< sSfM_Data_Filename << "\" cannot be read." << std::endl;
-    return EXIT_FAILURE;
-  }
-
   // Read the matches
-  std::shared_ptr<Matches_Provider> matches_provider = std::make_shared<Matches_Provider>();
-  if (!matches_provider->load(sfm_data, sMatchFile)) {
+  matching::PairWiseMatches pairWise_matches;
+  if (!Load(pairWise_matches, sMatchFile)) {
     std::cerr << "\nInvalid matches file." << std::endl;
     return EXIT_FAILURE;
   }
 
   // Write the matches
-  if (!Save(matches_provider->pairWise_matches_, sOutMatchFile))
+  if (!Save(pairWise_matches, sOutMatchFile))
   {
     std::cerr
         << "Cannot save matches to: "
