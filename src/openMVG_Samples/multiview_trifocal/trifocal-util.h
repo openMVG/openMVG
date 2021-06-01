63a0395cf80c341bccca54d2cb3c3d2d2c10583b
Ricardo Fabbri <rfabbri@gmail.com>
2021-06-01 15:13:00 -0300

    pix to px for pixel coords

--- src/openMVG_Samples/multiview_trifocal/trifocal-util.h
+++ src/openMVG_Samples/multiview_trifocal/trifocal-util.h
@@ -12,25 +12,25 @@ namespace trifocal3pt {
 void
 revert_intrinsics(
     const double K[/*3 or 2 ignoring last line*/][3], 
-    double pix_coords[2], 
+    double px_coords[2], 
     const double normalized_coords[2]);
 
 void
 revert_intrinsics_tgt(
     const double K[/*3 or 2 ignoring last line*/][3], 
-    double pix_tgt_coords[2], 
+    double px_tgt_coords[2], 
     const double normalized_tgt_coords[2]);
 
 void
 invert_intrinsics(
     const double K[/*3 or 2 ignoring last line*/][3], 
-    const double pix_coords[2], 
+    const double px_coords[2], 
     double normalized_coords[2]);
 
 void
 invert_intrinsics_tgt(
     const double K[/*3 or 2 ignoring last line*/][3], 
-    const double pix_tgt_coords[2], 
+    const double px_tgt_coords[2], 
     double normalized_tgt_coords[2]);
 
 } // namespace trifocal3pt
