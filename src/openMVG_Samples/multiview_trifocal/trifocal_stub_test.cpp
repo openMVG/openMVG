a6b13fb0f758429f58fa8cef77bfcfa7ed337b8c
Gabriel C. Andrade <gabrielandradecode@outlook.com>
2021-11-12 10:52:11 -0300

    commiting updated stub test

---
+++ src/openMVG_Samples/multiview_trifocal/trifocal_stub_test.cpp
@@ -0,0 +1,13 @@
+#include "testing/testing.h"
+
+
+TEST(trifocal_app, blele)
+{
+  CHECK(true);
+}
+
+
+/* ************************************************************************* */
+int main() { TestResult tr; return TestRegistry::runAllTests(tr);}
+/* ************************************************************************* */
+
