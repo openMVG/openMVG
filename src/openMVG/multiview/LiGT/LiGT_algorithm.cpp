e86d3cb773f163984d7958e361059c24ef6305da
pmoulon <pmoulon@gmail.com>
2022-04-07 09:36:32 -0700

    Fix Eigen solution in Identify Sign

--- src/openMVG/multiview/LiGT/LiGT_algorithm.cpp
+++ src/openMVG/multiview/LiGT/LiGT_algorithm.cpp
@@ -352,25 +352,11 @@ void LiGTProblem::WriteTime() {
 
 void LiGTProblem::IdentifySign(const MatrixXd& A_lr,
                                VectorXd& evectors) {
-    VectorXd judgeValue = A_lr * evectors;
-//    int positive_count = (judgeValue.array() > 0).sum();
-//    int negative_count = judgeValue.size() - positive_count;
-
-    int positive_count = 0;
-    int negative_count = 0;
-
-    for (int i = 0; i < judgeValue.rows(); ++i){
-        if (judgeValue[i]>0)
-            positive_count++;
-        else
-            negative_count++;
-    }
-
-    OPENMVG_LOG_INFO << "positive count = "<< positive_count<<", negative count = "<< negative_count;
-
+    auto judgeValue = A_lr * evectors;
+    int positive_count = (judgeValue.array() > 0.0).cast<int>().sum();
+    int negative_count = judgeValue.rows() - positive_count;
     if (positive_count < negative_count) {
         evectors = -evectors;
-        OPENMVG_LOG_INFO << "signs are reversed!";
     }
 }
 
@@ -393,11 +379,11 @@ void LiGTProblem::SelectBaseViews(const Track& track,
             const Vector3d& i_coord = track[i].coord;
             const Vector3d& j_coord = track[j].coord;
 
-            Matrix3d& R_i = global_rotations_[i_view_id];
-            Matrix3d& R_j = global_rotations_[j_view_id];
+            const Matrix3d& R_i = global_rotations_[i_view_id];
+            const Matrix3d& R_j = global_rotations_[j_view_id];
 
-            Matrix3d R_ij = R_j * R_i.transpose();
-            Vector3d theta_ij = j_coord.cross(R_ij * i_coord);
+            const Matrix3d R_ij = R_j * R_i.transpose();
+            const Vector3d theta_ij = j_coord.cross(R_ij * i_coord);
 
             double criterion_value = theta_ij.norm();
 
@@ -421,95 +407,95 @@ void LiGTProblem::BuildLTL(Eigen::MatrixXd& LTL,
     Eigen::MatrixXd tmp_LiGT_vec = Eigen::MatrixXd::Zero(3, num_view_ * 3);
 
 #ifdef OPENMVG_USE_OPENMP
-        #pragma omp parallel for shared(A_lr, LTL)
+    #pragma omp parallel for shared(A_lr, LTL)
 #endif
-        for (PtsId track_id = 0; track_id < tracks_.size(); ++track_id) {
-            TrackInfo track_info = tracks_[track_id];
-            Track track = track_info.track;
+    for (PtsId track_id = 0; track_id < tracks_.size(); ++track_id) {
+        const TrackInfo& track_info = tracks_[track_id];
+        const Track& track = track_info.track;
 
-            ViewId lbase_view_id = 0;
-            ViewId rbase_view_id = 0;
+        ViewId lbase_view_id = 0;
+        ViewId rbase_view_id = 0;
 
-            ObsId id_lbase = 0;
-            ObsId id_rbase = 0;
+        ObsId id_lbase = 0;
+        ObsId id_rbase = 0;
 
-            // [Step.2 in Pose-only algorithm]: select left/right-base views
-            SelectBaseViews(track,
-                            lbase_view_id,
-                            rbase_view_id,
-                            id_lbase,
-                            id_rbase);
+        // [Step.2 in Pose-only algorithm]: select left/right-base views
+        SelectBaseViews(track,
+                        lbase_view_id,
+                        rbase_view_id,
+                        id_lbase,
+                        id_rbase);
 
-            // [Step.3 in Pose-only algorithm]: calculate local L matrix,
-            for (ObsId i = 0; i < track.size(); i++) {
-                ViewId i_view_id = track[i].view_id; // the current view id
+        // [Step.3 in Pose-only algorithm]: calculate local L matrix,
+        for (ObsId i = 0; i < track.size(); i++) {
+            ViewId i_view_id = track[i].view_id; // the current view id
 
-                if (i_view_id != lbase_view_id) {
-                    auto xi_cross = CrossProductMatrix(track[i].coord);
-                    auto R_li = global_rotations_[i_view_id] * global_rotations_[lbase_view_id].transpose();
-                    auto R_lr = global_rotations_[rbase_view_id] * global_rotations_[lbase_view_id].transpose();
+            if (i_view_id != lbase_view_id) {
+                auto xi_cross = CrossProductMatrix(track[i].coord);
+                auto R_li = global_rotations_[i_view_id] * global_rotations_[lbase_view_id].transpose();
+                auto R_lr = global_rotations_[rbase_view_id] * global_rotations_[lbase_view_id].transpose();
 
-                    auto tmp_a_lr = CrossProductMatrix(R_lr * track[id_lbase].coord)
-                            * track[id_rbase].coord;
+                auto tmp_a_lr = CrossProductMatrix(R_lr * track[id_lbase].coord)
+                        * track[id_rbase].coord;
 
-                    // a_lr (Row) vector in a_lr * t > 0
-                    auto a_lr = tmp_a_lr.transpose() * CrossProductMatrix(track[id_rbase].coord);
+                // a_lr (Row) vector in a_lr * t > 0
+                auto a_lr = tmp_a_lr.transpose() * CrossProductMatrix(track[id_rbase].coord);
 
-                    // combine all a_lr vectors into a matrix form A, i.e., At > 0
-                    A_lr.row(track_id).block<1, 3>(0, lbase_view_id * 3) = a_lr * global_rotations_[rbase_view_id];
-                    A_lr.row(track_id).block<1, 3>(0, rbase_view_id * 3) = -a_lr * global_rotations_[rbase_view_id];
+                // combine all a_lr vectors into a matrix form A, i.e., At > 0
+                A_lr.row(track_id).block<1, 3>(0, lbase_view_id * 3) = a_lr * global_rotations_[rbase_view_id];
+                A_lr.row(track_id).block<1, 3>(0, rbase_view_id * 3) = -a_lr * global_rotations_[rbase_view_id];
 
-                    // theta_lr
-                    auto theta_lr_vector = CrossProductMatrix(track[id_rbase].coord)
-                            * R_lr
-                            * track[id_lbase].coord;
+                // theta_lr
+                auto theta_lr_vector = CrossProductMatrix(track[id_rbase].coord)
+                        * R_lr
+                        * track[id_lbase].coord;
 
-                    double theta_lr = theta_lr_vector.squaredNorm();
+                double theta_lr = theta_lr_vector.squaredNorm();
 
-                    // caculate matrix B [rbase_view_id]
-                    auto Coefficient_B =
-                            xi_cross * R_li * track[id_lbase].coord * a_lr * global_rotations_[rbase_view_id];
+                // caculate matrix B [rbase_view_id]
+                auto Coefficient_B =
+                        xi_cross * R_li * track[id_lbase].coord * a_lr * global_rotations_[rbase_view_id];
 
-                    // caculate matrix C [i_view_id]
-                    auto Coefficient_C =
-                            theta_lr * CrossProductMatrix(track[i].coord) * global_rotations_[i_view_id];
+                // caculate matrix C [i_view_id]
+                auto Coefficient_C =
+                        theta_lr * CrossProductMatrix(track[i].coord) * global_rotations_[i_view_id];
 
-                    // caculate matrix D [lbase_view_id]
-                    auto Coefficient_D = -(Coefficient_B + Coefficient_C);
+                // caculate matrix D [lbase_view_id]
+                auto Coefficient_D = -(Coefficient_B + Coefficient_C);
 
-                    // calculate temp matrix L for a single 3D matrix
-                    tmp_LiGT_vec.setZero();
+                // calculate temp matrix L for a single 3D matrix
+                tmp_LiGT_vec.setZero();
 
-                    tmp_LiGT_vec.block<3, 3>(0, rbase_view_id * 3) += Coefficient_B;
-                    tmp_LiGT_vec.block<3, 3>(0, i_view_id * 3) += Coefficient_C;
-                    tmp_LiGT_vec.block<3, 3>(0, lbase_view_id * 3) += Coefficient_D;
-
-
-                    // calculate LtL submodule
-                    auto LTL_l_row = Coefficient_D.transpose() * tmp_LiGT_vec;
-                    auto LTL_r_row = Coefficient_B.transpose() * tmp_LiGT_vec;
-                    auto LTL_i_row = Coefficient_C.transpose() * tmp_LiGT_vec;
+                tmp_LiGT_vec.block<3, 3>(0, rbase_view_id * 3) += Coefficient_B;
+                tmp_LiGT_vec.block<3, 3>(0, i_view_id * 3) += Coefficient_C;
+                tmp_LiGT_vec.block<3, 3>(0, lbase_view_id * 3) += Coefficient_D;
 
 
-                    // assignment for LtL (except for the reference view id)
+                // calculate LtL submodule
+                auto LTL_l_row = Coefficient_D.transpose() * tmp_LiGT_vec;
+                auto LTL_r_row = Coefficient_B.transpose() * tmp_LiGT_vec;
+                auto LTL_i_row = Coefficient_C.transpose() * tmp_LiGT_vec;
+
+
+                // assignment for LtL (except for the reference view id)
 #ifdef OPENMVG_USE_OPENMP
-                    #pragma omp critical
+                #pragma omp critical
 #endif
-                    {
-                        if(lbase_view_id > 0 )
-                        LTL.middleRows<3>(lbase_view_id * 3 - 3) += LTL_l_row.rightCols(LTL_l_row.cols() - 3);
+                {
+                    if(lbase_view_id > 0 )
+                    LTL.middleRows<3>(lbase_view_id * 3 - 3) += LTL_l_row.rightCols(LTL_l_row.cols() - 3);
 
-                        if(rbase_view_id > 0 )
-                        LTL.middleRows<3>(rbase_view_id * 3 - 3) += LTL_r_row.rightCols(LTL_r_row.cols() - 3);
+                    if(rbase_view_id > 0 )
+                    LTL.middleRows<3>(rbase_view_id * 3 - 3) += LTL_r_row.rightCols(LTL_r_row.cols() - 3);
 
-                        if(i_view_id > 0 )
-                        LTL.middleRows<3>(i_view_id * 3 - 3) += LTL_i_row.rightCols(LTL_i_row.cols() - 3);
-                    }
-
+                    if(i_view_id > 0 )
+                    LTL.middleRows<3>(i_view_id * 3 - 3) += LTL_i_row.rightCols(LTL_i_row.cols() - 3);
                 }
-            }
 
+            }
         }
+
+    }
 
 }
 
