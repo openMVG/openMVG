93f64d4c67e0d1330fa970e6bfb2e1070e1f7011
Bouke van der Bijl <i@bou.ke>
2023-01-23 14:09:30 +0100

    Remove usage of unordered_map for clang
    It seems to just cause a bunch of headache for unclear benefit.

--- src/openMVG/types.hpp
+++ src/openMVG/types.hpp
@@ -9,44 +9,13 @@
 #ifndef OPENMVG_TYPES_HPP
 #define OPENMVG_TYPES_HPP
 
-#ifndef OPENMVG_STD_UNORDERED_MAP
-
 #include <Eigen/Core>
-
-#endif
-
 #include <cstdint>
 #include <functional>
 #include <limits>
 #include <map>
 #include <set>
 #include <vector>
-
-#ifdef OPENMVG_STD_UNORDERED_MAP
-
-#include <algorithm>
-#include <unordered_map>
-#include <utility>
-
-#include "openMVG/stl/hash.hpp"
-namespace std {
-  template<typename T1, typename T2>
-  struct hash<std::pair<T1, T2>> {
-    std::size_t operator()(std::pair<T1, T2> const &p) const {
-      std::size_t seed1(0);
-      stl::hash_combine(seed1, p.first);
-      stl::hash_combine(seed1, p.second);
-
-      std::size_t seed2(0);
-      stl::hash_combine(seed2, p.second);
-      stl::hash_combine(seed2, p.first);
-
-      return std::min(seed1, seed2);
-    }
-  };
-}
-
-#endif // OPENMVG_STD_UNORDERED_MAP
 
 /**
 * @brief Main namespace of openMVG API
@@ -69,18 +38,6 @@ using Pair_Set = std::set<Pair>;
 /// Vector of Pair
 using Pair_Vec = std::vector<Pair>;
 
-#if defined OPENMVG_STD_UNORDERED_MAP
-
-/**
-* @brief Standard Hash_Map class
-* @tparam K type of the keys
-* @tparam V type of the values
-*/
-template<typename Key, typename Value>
-using Hash_Map = std::unordered_map<Key, Value>;
-
-#else
-
 /**
 * @brief Standard Hash_Map class
 * @tparam K type of the keys
@@ -89,8 +46,6 @@ using Hash_Map = std::unordered_map<Key, Value>;
 template<typename Key, typename Value>
 using Hash_Map = std::map<Key, Value, std::less<Key>,
   Eigen::aligned_allocator<std::pair<const Key, Value>>>;
-
-#endif // OPENMVG_STD_UNORDERED_MAP
 
 } // namespace openMVG
 
