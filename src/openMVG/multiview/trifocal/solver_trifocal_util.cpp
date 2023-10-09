25e1860b726d20e342806f356107392924bbddcc
Ricardo Fabbri <rfabbri@gmail.com>
2023-10-08 22:22:59 -0300

    [Trifocal+P2Pt] missing file

---
+++ src/openMVG/multiview/trifocal/solver_trifocal_util.cpp
@@ -0,0 +1,38 @@
+// This file is part of OpenMVG, an Open Multiple View Geometry C++ library.
+//
+//:\file
+//\author Ricardo Fabbri Rio de Janeiro State U. (rfabbri.github.io) 
+//\author Pierre MOULON
+//\author Gabriel ANDRADE Rio de Janeiro State U.
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "openMVG/multiview/trifocal/solver_trifocal_util.hpp"
+
+namespace openMVG {
+namespace trifocal {
+  
+bool
+probe_solutions(
+    const std::vector<trifocal_model_t> &solutions, 
+    trifocal_model_t &gt, 
+    unsigned *solution_index)
+{
+  double cameras_quat[M::nsols][M::nve];
+
+  // translate trifocal_model from RC to QT (quaternion-translation)
+  // - for each solution
+  // -   translate to internal matrix form
+  // -   call RC to QT
+
+  tt2qt(gt, data::cameras_gt_quat_);
+  for (unsigned s=0; s < solutions.size(); ++s)
+    tt2qt(solutions[s], cameras_quat[s]);
+  
+  return io14::probe_all_solutions_quat(cameras_quat, data::cameras_gt_quat_, solutions.size(), solution_index);
+}
+
+} // namespace trifocal
+} // namespace OpenMVG
