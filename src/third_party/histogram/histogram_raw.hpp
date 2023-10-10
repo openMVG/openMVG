65b1e32e2db6bd84334b251938658bd7f1adae98
Ricardo Fabbri <rfabbri@gmail.com>
2023-10-10 14:51:59 -0300

    [Third-Party] histogram variant without anonymous namespace
    Allowing for modularizaiton across translation units.

---
+++ src/third_party/histogram/histogram_raw.hpp
@@ -0,0 +1,14 @@
+#ifndef HISTOGRAMMING_RAW_H
+#define HISTOGRAMMING_RAW_H
+
+#include <iomanip>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// Same as histogrma.hpp, but without the anynymous namespace
+
+#include "histogram_core.hpp"
+
+#endif HISTOGRAMMING_RAW_H
