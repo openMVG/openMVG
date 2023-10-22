f663685980f01b121d254255ea95eba22e8622c7
Ricardo Fabbri <rfabbri@gmail.com>
2023-10-22 17:32:34 -0300

    [Trifocal+P2Pt] stack stratch memory slightly faster than global

--- src/openMVG/multiview/solver_resection_p2pt_fabbri.cpp
+++ src/openMVG/multiview/solver_resection_p2pt_fabbri.cpp
@@ -63,6 +63,8 @@ class p2pt { // fully static, not to be instantiated - just used for templating
   );
 };
 
+// polynomial in t
+// solution is pose(t)
 template<typename T>
 struct pose_poly {
 	T A0, A1, A2, B0, B1, B2, B3, C0, C1, C2, C3, C4,
@@ -123,23 +125,22 @@ struct pose_poly {
 		const T Gama1[3], const T Tgt1[3], const T Gama2[3], const T Tgt2[3]);
   
 	inline void find_bounded_root_intervals(bool (*root_ids_out)[ROOT_IDS_LEN]) {
-    T p[10]; 
-    T curr_val = fn_t(t_vec(0),p), next_val;
+    T p[10];
+    T curr_val = fn_t(t_vec(0), p), next_val;
     for (unsigned i = 0; i < ROOT_IDS_LEN; i++) {
-      next_val = fn_t(t_vec(i+1),p);
+      next_val = fn_t(t_vec(i+1), p);
        (*root_ids_out)[i] = curr_val * next_val < 0;
       curr_val = next_val;
     }
   }
   
-	inline T fn_t(const T t, T o[10]) {  //o: output
-    T &A = o[0]; T &B = o[1]; T &C = o[2]; T &E = o[3]; T &F = o[4]; 
-    T &G = o[5]; T &H = o[6]; T &J = o[7]; T &K = o[8]; T &L = o[9];
-
+	inline T fn_t(const T t, T p[10]) {
     //%function of t part :
     const T t2 = t*t, t3 = t2*t, t4 = t3*t, t5 = t4*t, t6 = t5*t, t7 = t6*t, t8 = t7*t;
     const T t2p12 = (t2 + 1.) * (t2 + 1.), t2p13 = t2p12 * (t2 + 1.), t2p14 = t2p13 * (t2 + 1.);
 
+    T &A = p[0]; T &B = p[1]; T &C = p[2]; T &E = p[3]; T &F = p[4]; 
+    T &G = p[5]; T &H = p[6]; T &J = p[7]; T &K = p[8]; T &L = p[9];
     A = (A0+A1*t+A2*t2-A1*t3+A0*t4)/t2p12;
     B = (B0+B1*t+B2*t2+B3*t3-B2*t4+B1*t5-B0*t6)/t2p13;
     C = (C0+C1*t+C2*t2+C3*t3+C4*t4-C3*t5+C2*t6-C1*t7+C0*t8)/t2p14;
@@ -148,24 +149,25 @@ struct pose_poly {
     G = (G0+G1*t+G2*t2+G3*t3+G4*t4-G3*t5+G2*t6-G1*t7+G0*t8)/t2p14;
     H = (H0+H1*t+H2*t2+H3*t3+H4*t4-H3*t5+H2*t6-H1*t7+H0*t8)/t2p14;
     J = (J0+J1*t+J2*t2+J3*t3-J2*t4+J1*t5-J0*t6)/t2p13;
-    K = (K0+K1*t+K2*t2+K3*t3-K2*t4+K1*t5-K0*t6)/t2p13; // <<<<<<<<<<
-    L = (L0+L1*t+L2*t2-L1*t3+L0*t4)/t2p12; // OK
+    K = (K0+K1*t+K2*t2+K3*t3-K2*t4+K1*t5-K0*t6)/t2p13;
+    L = (L0+L1*t+L2*t2-L1*t3+L0*t4)/t2p12;
 
-//    OPENMVG_LOG_INFO << std::setprecision(20) <<  "t: " << t << std::endl <<
-//    "A: "<< A << std::endl << 
-//    "B: "<< B << std::endl << 
-//    "C: "<< C << std::endl << 
-//    "E: "<< E << std::endl << 
-//    "F: "<< F << std::endl << 
-//    "G: "<< G << std::endl << 
-//    "H: "<< H << std::endl << 
-//    "J: "<< J << std::endl << 
-//    "K: "<< K << std::endl << 
-//    "L: "<< L << std::endl;
+    //    OPENMVG_LOG_INFO << std::setprecision(20) <<  "t: " << t << std::endl <<
+    //    "A: "<< A << std::endl << 
+    //    "B: "<< B << std::endl << 
+    //    "C: "<< C << std::endl << 
+    //    "E: "<< E << std::endl << 
+    //    "F: "<< F << std::endl << 
+    //    "G: "<< G << std::endl << 
+    //    "H: "<< H << std::endl << 
+    //    "J: "<< J << std::endl << 
+    //    "K: "<< K << std::endl << 
+    //    "L: "<< L << std::endl;
 
     const T A2=A*A, B2=B*B, C2=C*C, E2=E*E, F2=F*F, G2=G*G, H2=H*H, 
             H3=H2*H, H4=H3*H, J2=J*J, J3=J2*J, K2=K*K, K3=K2*K, L2=L*L, L3=L2*L;
     
+    // XXX terms that have a number can be precomputed, independ of t
     return E2*B2*H2*J2 +G2*C2*L*L*L*L +G2*A2*K3*K +E2*A2*H4 +E2*C2*J3*J
     -2.*E*A*H2*G*C*L2 +2.*E2*A*H2*C*J2 -2.*E2*C*J3*B*H +2.*E*C2*J2*G*L2
     +2.*E*A2*H2*G*K2 -2.*E2*A*H3*B*J -2.*E*A*H2*G*B*K*L -2.*E*C*J2*G*B*K*L
@@ -181,16 +183,15 @@ struct pose_poly {
     -F2*C*L2*B*H*J +G*E*B2*H2*L2 +G*E*B2*K2*J2 +8.*G*E*A*H*K*C*J*L;
   }
   
-	inline T fn_t(const T t) { T b[10]; return fn_t(t, b); }
-	inline T operator()(T t) { return fn_t(t); }
+	// inline T operator()(T t) { return fn_t(t); }
   
 	inline void rhos_from_root_ids(const bool (&root_ids)[ROOT_IDS_LEN], 
                                  T (*out)[3][ROOT_IDS_LEN], 
                                  unsigned *out_ts_len) {
     
     T (&ts)[ROOT_IDS_LEN] = (*out)[0];
+    T p[10];
     unsigned &ts_end = *out_ts_len; ts_end = 0;
-    T p[10]; 
     for (unsigned i = 0; i < ROOT_IDS_LEN; i++) {
       if (!root_ids[i]) continue;
       T t0 = t_vec(i), t1 = t_vec(i+1), &t2 = ts[ts_end++];
